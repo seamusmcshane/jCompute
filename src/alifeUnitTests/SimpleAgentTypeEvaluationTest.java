a0dca44705310dfeed3c3fb1111175af75c14265
Seamus McShane <seamus.mcshane@hotmail.com>
2012-04-13 13:21:28 +0100

    SimpleAgentTypeEvaluationTest object names made less generic for readablity.

--- src/alifeUnitTests/SimpleAgentTypeEvaluationTest.java
+++ src/alifeUnitTests/SimpleAgentTypeEvaluationTest.java
@@ -19,11 +19,11 @@ import alife.World;
 
 public class SimpleAgentTypeEvaluationTest
 {
-	SimpleAgentType agent1;
-	SimpleAgentType agent2;
-	SimpleAgentType agent3;
-	SimpleAgentType agent4;
-
+	
+	SimpleAgentType predator1;
+	SimpleAgentType prey1;
+	SimpleAgentType predator2;
+	SimpleAgentType prey2;
 	
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception
@@ -34,55 +34,55 @@ public class SimpleAgentTypeEvaluationTest
 	@Before
 	public void setUp() throws Exception
 	{
-		agent1 = new SimpleAgentType(AgentType.PREDATOR);
-		agent2 = new SimpleAgentType(AgentType.PREY);
-		agent3 = new SimpleAgentType(AgentType.PREDATOR);
-		agent4 = new SimpleAgentType(AgentType.PREY);		
+		predator1 = new SimpleAgentType(AgentType.PREDATOR);
+		prey1 = new SimpleAgentType(AgentType.PREY);
+		predator2 = new SimpleAgentType(AgentType.PREDATOR);
+		prey2 = new SimpleAgentType(AgentType.PREY);		
 	}
 	
 	@Test
 	public void predatorVsPredatorIsSameType()
 	{
-		assertEquals(true,agent1.isSameType(agent3));
+		assertEquals(true,predator1.isSameType(predator2));
 	}
 	
 	@Test
 	public void predatorVsPreyIsSameType()
 	{
-		assertEquals(false,agent1.isSameType(agent2));
+		assertEquals(false,predator1.isSameType(prey1));
 	}
 	
 	
 	@Test
 	public void preyVsPredatorIsSameType()
 	{
-		assertEquals(false,agent2.isSameType(agent1));
+		assertEquals(false,prey1.isSameType(predator1));
 	}
 	
 	
 	@Test
 	public void preYVsPreyIsSameType()
 	{
-		assertEquals(true,agent2.isSameType(agent4));
+		assertEquals(true,prey1.isSameType(prey2));
 	}
 
 	@Test
 	public void predatorEatsPlants()
 	{
-		assertEquals(false,agent1.eatsPlants());
+		assertEquals(false,predator1.eatsPlants());
 	}	
 	
 	@Test
 	public void preyEatsPlants()
 	{
-		assertEquals(true,agent2.eatsPlants());
+		assertEquals(true,prey1.eatsPlants());
 	}
 	
 	@Test
 	public void PredatorvsPredatorStrongerThanTests()
 	{
 		/* Predator vs Predator */
-		assertEquals(AgentEval.SAME,agent1.strongerThan(agent3));
+		assertEquals(AgentEval.SAME,predator1.strongerThan(predator2));
 	}
 	
 	
@@ -90,7 +90,7 @@ public class SimpleAgentTypeEvaluationTest
 	public void PreyVsPreyStrongerThanTests()
 	{
 		/* Prey vs Prey */
-		assertEquals(AgentEval.SAME,agent2.strongerThan(agent4));
+		assertEquals(AgentEval.SAME,prey1.strongerThan(prey2));
 	}
 	
 	
@@ -98,7 +98,7 @@ public class SimpleAgentTypeEvaluationTest
 	public void PredatorvsPreyStrongerThanTests()
 	{		
 		/* Predator vs Prey */
-		assertEquals(AgentEval.STRONGER,agent1.strongerThan(agent2));
+		assertEquals(AgentEval.STRONGER,predator1.strongerThan(prey1));
 	}
 	
 	
@@ -106,19 +106,19 @@ public class SimpleAgentTypeEvaluationTest
 	public void PreyvsPredatorStrongerThanTests()
 	{		
 		/* Prey vs Predator */
-		assertEquals(AgentEval.WEAKER,agent2.strongerThan(agent1));
+		assertEquals(AgentEval.WEAKER,prey1.strongerThan(predator1));
 	}
 	
 	@Test
 	public void PredatorType()
 	{
-		assertEquals(AgentType.PREDATOR,agent1.getType());
+		assertEquals(AgentType.PREDATOR,predator1.getType());
 	}
 	
 	@Test
 	public void PreyType()
 	{
-		assertEquals(AgentType.PREY,agent2.getType());
+		assertEquals(AgentType.PREY,prey1.getType());
 	}
 	
 
