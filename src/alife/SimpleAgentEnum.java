b8159d59cb2d9bd23fe8212e46f9247464f7a709
Seamus McShane <seamus.mcshane@hotmail.com>
2012-04-13 13:20:59 +0100

    Simple Agent Enum documented.

--- src/alife/SimpleAgentEnum.java
+++ src/alife/SimpleAgentEnum.java
@@ -1,13 +1,16 @@
 package alife;
 /**
- * A Small enumerate type class.
+ * A Small Enumerate container class for Agents.
  * Used to avoid magic numbers for types and states.
  */
 public class SimpleAgentEnum
 {
+	/** Agent Type Enum */
 	public enum AgentType{PREY,PREDATOR};
 	
+	/** Agent State Enum */
 	public enum AgentState{ROAM,HUNT,EVADE,GRAZE};
 	
+	/** Agent Types Relative Strengths */
 	public enum AgentEval{SAME,STRONGER,WEAKER};
 }
