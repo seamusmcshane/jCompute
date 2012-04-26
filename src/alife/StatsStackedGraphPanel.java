126972f4d776291632fb911b0bbba4207960ec10
Seamus McShane <seamus.mcshane@hotmail.com>
2012-04-26 15:42:32 +0100

    World and StatsStackedGraphPanel tidy up.

--- src/alife/StatsStackedGraphPanel.java
+++ src/alife/StatsStackedGraphPanel.java
@@ -15,20 +15,20 @@ public class StatsStackedGraphPanel extends JPanel
 	private int plantsSamples[];
 	private int preySamples[];
 	private int predSamples[];
-	
+
 	private int graphX;
 	private int graphY;
 	private int graphWidth;
 	private int graphHeight;
-	
+
 	private float sampleNum;
-	
-	private float graphSamples=1;
-		
+
+	private float graphSamples = 1;
+
 	private float plantMax = 0;
 	private float preyMax = 0;
 	private float predMax = 0;
-	
+
 	/**
 	 * A new Graph Panel
 	 */
@@ -37,17 +37,17 @@ public class StatsStackedGraphPanel extends JPanel
 
 	}
 
-	public void setSampleArrays(int plantsSamples[],	int preySamples[], int predSamples[], int sampleNum)
+	public void setSampleArrays(int plantsSamples[], int preySamples[], int predSamples[], int sampleNum)
 	{
 		this.plantsSamples = plantsSamples;
 		this.preySamples = preySamples;
 		this.predSamples = predSamples;
-		
-		graphSamples=1;
-		
-		this.sampleNum = sampleNum;		
+
+		graphSamples = 1;
+
+		this.sampleNum = sampleNum;
 	}
-	
+
 	/**
 	 * Updates the graph and draws it on an interval based on currSampleNum.
 	 * @param plantMax
@@ -55,33 +55,33 @@ public class StatsStackedGraphPanel extends JPanel
 	 * @param predMax
 	 * @param currSampleNum
 	 */
-	public void updateGraph(float plantMax,float preyMax, float predMax,int currSampleNum)
+	public void updateGraph(float plantMax, float preyMax, float predMax, int currSampleNum)
 	{
 		this.plantMax = plantMax;
-		
+
 		this.preyMax = preyMax;
-		
+
 		this.predMax = predMax;
-		
-		if(currSampleNum<sampleNum)
+
+		if (currSampleNum < sampleNum)
 		{
-			graphSamples=currSampleNum;
+			graphSamples = currSampleNum;
 		}
 		else
 		{
-			graphSamples=sampleNum;
-		}		
+			graphSamples = sampleNum;
+		}
 
 	}
-		
+
 	/** 
 	 * Does the plants or agents have the greater point
 	 * Max is tied to the greater */
 	public void tieAllMax()
 	{
 		float agent_max = tiePredPreyMax();
-		
-		if(plantMax > agent_max)
+
+		if (plantMax > agent_max)
 		{
 			predMax = plantMax;
 			preyMax = plantMax;
@@ -89,16 +89,16 @@ public class StatsStackedGraphPanel extends JPanel
 		else
 		{
 			plantMax = agent_max;
-		}	
+		}
 	}
-	
+
 	/**
 	 * Which of the agents has the greater max point in the graph 
 	 * max is tied to the greater, also returns max for further evaluation */
 	public float tiePredPreyMax()
-	{		
+	{
 		float max;
-		if(preyMax > predMax)
+		if (preyMax > predMax)
 		{
 			max = predMax = preyMax;
 		}
@@ -106,100 +106,100 @@ public class StatsStackedGraphPanel extends JPanel
 		{
 			max = predMax = preyMax;
 		}
-		
+
 		return max;
 	}
-	
+
 	/** Draws the graph */
 	public void paintComponent(Graphics g)
 	{
 		super.paintComponent(g);
-				
-		Graphics2D g2 = (Graphics2D) g;	
-	
+
+		Graphics2D g2 = (Graphics2D) g;
+
 		// Too Slow for this graph
-	   // g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,RenderingHints.VALUE_ANTIALIAS_ON);		    
+		// g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,RenderingHints.VALUE_ANTIALIAS_ON);		    
 
 		calculateGraphSize();
-		
+
 		drawSamples(g2);
 	}
 
 	/** Draws the graph lines */
 	public void drawSamples(Graphics2D g2)
-	{				
-		if(plantsSamples == null || preySamples == null || predSamples == null)
+	{
+		if (plantsSamples == null || preySamples == null || predSamples == null)
 		{
 			return;
 		}
-		
-		float scaleWidthInterval = graphWidth/ sampleNum;
-		
-		float total_height_scale=0;
-		
-		float total_val=0;
-		
+
+		float scaleWidthInterval = graphWidth / sampleNum;
+
+		float total_height_scale = 0;
+
+		float total_val = 0;
+
 		int predSampleXVal = 0;
-		int predSampleYVal = 0;	
-		
+		int predSampleYVal = 0;
+
 		int plantsSampleXVal = 0;
-		int plantsSampleYVal = 0;	
+		int plantsSampleYVal = 0;
 
 		int preySampleXVal = 0;
-		int preySampleYVal = 0;	
-				
+		int preySampleYVal = 0;
+
 		/* Loops through all three sample arrays */
-		for(int i =0;i<sampleNum;i++)
-		{			
-			
-			total_val = plantsSamples[i]+preySamples[i]+predSamples[i];
-			
-			total_height_scale = graphHeight/(total_val+1);
-				
+		for (int i = 0; i < sampleNum; i++)
+		{
+
+			total_val = plantsSamples[i] + preySamples[i] + predSamples[i];
+
+			total_height_scale = graphHeight / (total_val + 1);
+
 			/* Plants */
-			plantsSampleXVal = (int) (i*scaleWidthInterval);					
-			plantsSampleYVal = ((int)(plantsSamples[i]*total_height_scale));													
-							
+			plantsSampleXVal = (int) (i * scaleWidthInterval);
+			plantsSampleYVal = ((int) (plantsSamples[i] * total_height_scale));
+
 			/* Prey */
-			preySampleXVal = (int) (i*scaleWidthInterval);					
-			preySampleYVal = ((int)(preySamples[i]*total_height_scale));														
-				
+			preySampleXVal = (int) (i * scaleWidthInterval);
+			preySampleYVal = ((int) (preySamples[i] * total_height_scale));
+
 			/* Predator */
-			predSampleXVal = (int) (i*scaleWidthInterval);					
-			predSampleYVal = ((int)(predSamples[i]*total_height_scale));		
-			
-			if(preySamples[i]>0)
+			predSampleXVal = (int) (i * scaleWidthInterval);
+			predSampleYVal = ((int) (predSamples[i] * total_height_scale));
+
+			if (preySamples[i] > 0)
 			{
-				g2.setColor(Color.GREEN);				
-				g2.drawLine(plantsSampleXVal ,graphHeight, plantsSampleXVal,graphHeight-plantsSampleYVal);			
+				g2.setColor(Color.GREEN);
+				g2.drawLine(plantsSampleXVal, graphHeight, plantsSampleXVal, graphHeight - plantsSampleYVal);
 			}
-							
-			if(preySamples[i]>0)
+
+			if (preySamples[i] > 0)
 			{
 				g2.setColor(Color.BLUE);
-				g2.drawLine(preySampleXVal ,graphHeight-plantsSampleYVal, preySampleXVal,predSampleYVal);				
+				g2.drawLine(preySampleXVal, graphHeight - plantsSampleYVal, preySampleXVal, predSampleYVal);
 			}
-			
-			if(predSamples[i]>0)
+
+			if (predSamples[i] > 0)
 			{
 				g2.setColor(Color.RED);
-				g2.drawLine(predSampleXVal ,0, predSampleXVal,predSampleYVal);				
-			}						
+				g2.drawLine(predSampleXVal, 0, predSampleXVal, predSampleYVal);
+			}
 		}
-	}	
-		
+	}
+
 	public void drawGraph()
 	{
 		this.repaint();
 	}
-	
+
 	/** Gets the widths and height of this panel */
 	private void calculateGraphSize()
 	{
 		graphX = this.getX();
 		graphY = this.getY();
-		graphWidth = this.getWidth()-1;
-		graphHeight = this.getHeight()-2;
+		graphWidth = this.getWidth() - 1;
+		graphHeight = this.getHeight() - 2;
 	}
 
 }
--- src/alife/World.java
+++ src/alife/World.java
@@ -13,20 +13,20 @@ public class World
 {
 	/* The world grid background */
 	WorldGrid grid;
-	
+
 	/** The world boundary */
 	Rectangle world_bound;
 
 	/** The size of the world */
 	private static int world_size;
-	
+
 	@SuppressWarnings("static-access")
 	public World(int size)
 	{
 		this.world_size = size;
-		
-		world_bound = new Rectangle(0,0,world_size+1,world_size+1);	
-		
+
+		world_bound = new Rectangle(0, 0, world_size + 1, world_size + 1);
+
 		createGrid();
 	}
 
@@ -35,37 +35,36 @@ public class World
 	 * @param x
 	 * @param y
 	 */
-	public static boolean isBoundaryWall(float x,float y)
-	{		
+	public static boolean isBoundaryWall(float x, float y)
+	{
 		/* Top */
-		if(y<=0)
+		if (y <= 0)
 		{
 			return true;
 		}
-		
 
 		/* Bottom */
-		if(y>=world_size)
+		if (y >= world_size)
 		{
 			return true;
 		}
-		
+
 		/* Left */
-		if(x<=0)
+		if (x <= 0)
 		{
 			return true;
 		}
-		
+
 		/* Right */
-		if(x>=world_size)
+		if (x >= world_size)
 		{
 			return true;
 		}
-		
+
 		return false;
 
 	}
-	
+
 	/**
 	 * Is this the left or right of the world.
 	 * @param x
@@ -74,13 +73,13 @@ public class World
 	private static boolean checkXBoundary(float x)
 	{
 		/* Left */
-		if(x<=0)
+		if (x <= 0)
 		{
 			return true;
 		}
-		
+
 		/* Right */
-		if(x>=world_size)
+		if (x >= world_size)
 		{
 			return true;
 		}
@@ -95,37 +94,37 @@ public class World
 	private static boolean checkYBoundary(float y)
 	{
 		/* Left */
-		if(y<=0)
+		if (y <= 0)
 		{
 			return true;
 		}
-		
+
 		/* Right */
-		if(y>=world_size)
+		if (y >= world_size)
 		{
 			return true;
 		}
 		return false;
 	}
-	
+
 	/**
 	 *  Generate the world grid object
 	 */
 	private void createGrid()
 	{
-		int grid_steps=8;
-		grid = new WorldGrid(world_size,grid_steps);
+		int grid_steps = 8;
+		grid = new WorldGrid(world_size, grid_steps);
 	}
-	
+
 	/**
 	 *  Draw method for the world */
 	public void drawWorld(Graphics g)
 	{
-			grid.drawGrid(g);
-			
-			g.setColor(Color.blue);
-			
-			g.draw(world_bound);			
+		grid.drawGrid(g);
 
-	}	
+		g.setColor(Color.blue);
+
+		g.draw(world_bound);
+
+	}
 }
