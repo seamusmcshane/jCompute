e79d6e33034ca91df449ea816e0c4343b1b5d4a8
Seamus McShane <seamus.mcshane@hotmail.com>
2012-04-26 15:40:27 +0100

    GUI Change - Allow setting hunger threshold to 100 - always hungry.

--- src/alife/SimulationGUI.java
+++ src/alife/SimulationGUI.java
@@ -125,10 +125,10 @@ public class SimulationGUI
 	private static int predator_default_movecost_selected = 24; // 0.025
 
 	private static JComboBox comboBoxPreyHungerThres;
-	private static int prey_default_hungerthres_selected = 50; // 50
+	private static int prey_default_hungerthres_selected = 49; // 50
 
 	private static JComboBox comboBoxPredatorHungerThres;
-	private static int predator_default_hungerthres_selected = 50; // 50
+	private static int predator_default_hungerthres_selected = 49; // 50
 
 	private static JComboBox comboBoxPreyConsumptionRate;
 	private static int prey_default_consumptionrate_selected = 9; // Selects 9	
@@ -645,8 +645,8 @@ public class SimulationGUI
 		comboBoxPreyHungerThres.setToolTipText("The energy level at which Prey become hungry.");
 		comboBoxPreyHungerThres
 				.setModel(new DefaultComboBoxModel(new String[]
-				{"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48", "49", "50", "51", "52", "53", "54", "55", "56", "57", "58", "59", "60", "61", "62", "63", "64", "65", "66", "67", "68", "69", "70", "71", "72", "73", "74", "75", "76", "77", "78", "79", "80", "81", "82", "83", "84", "85", "86", "87", "88", "89", "90", "91", "92", "93", "94", "95", "96", "97",
-						"98", "99"}));
+				{"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48", "49", "50", "51", "52", "53", "54", "55", "56", "57", "58", "59", "60", "61", "62", "63", "64", "65", "66", "67", "68", "69", "70", "71", "72", "73", "74", "75", "76", "77", "78", "79", "80", "81", "82", "83", "84", "85", "86", "87", "88", "89", "90", "91", "92", "93", "94", "95", "96", "97",
+						"98", "99" , "100"}));
 		agentParamPanel.add(comboBoxPreyHungerThres);
 		comboBoxPreyHungerThres.setSelectedIndex(prey_default_hungerthres_selected);
 
@@ -654,8 +654,8 @@ public class SimulationGUI
 		comboBoxPredatorHungerThres.setToolTipText("The energy level at which Predators become hungry.");
 		comboBoxPredatorHungerThres
 				.setModel(new DefaultComboBoxModel(new String[]
-				{"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48", "49", "50", "51", "52", "53", "54", "55", "56", "57", "58", "59", "60", "61", "62", "63", "64", "65", "66", "67", "68", "69", "70", "71", "72", "73", "74", "75", "76", "77", "78", "79", "80", "81", "82", "83", "84", "85", "86", "87", "88", "89", "90", "91", "92", "93", "94", "95", "96", "97",
-						"98", "99"}));
+				{"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48", "49", "50", "51", "52", "53", "54", "55", "56", "57", "58", "59", "60", "61", "62", "63", "64", "65", "66", "67", "68", "69", "70", "71", "72", "73", "74", "75", "76", "77", "78", "79", "80", "81", "82", "83", "84", "85", "86", "87", "88", "89", "90", "91", "92", "93", "94", "95", "96", "97",
+						"98", "99" , "100"}));
 		agentParamPanel.add(comboBoxPredatorHungerThres);
 		comboBoxPredatorHungerThres.setSelectedIndex(predator_default_hungerthres_selected);
 
