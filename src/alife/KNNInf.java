daacf90761e5b8cabeb6bbabc8aecfb78a74632e
Seamus McShane <seamus.mcshane@outlook.com>
2013-04-01 20:56:18 +0100

    Small Tidy up of KNNInf

--- src/alife/KNNInf.java
+++ src/alife/KNNInf.java
@@ -9,18 +9,23 @@ import java.util.LinkedList;
  * @author Seamie
  *
  */
-public interface KNNInf <T>{
+public interface KNNInf <T>
+{
 	
+	/* Init the Tree */
 	void init(int dim);
 	
+	/* Add Item to Tree */
 	void add(int x, int y, SimpleAgent agent);
 	void add(float x, float y, SimpleAgent agent);
 	void add(double x, double y, SimpleAgent agent);	
 	
+	/* Find Nearest Item */
 	SimpleAgent nearestNeighbor(int x,int y);
 	SimpleAgent nearestNeighbor(float x,float y);
 	SimpleAgent nearestNeighbor(double x,double y);
 	
+	/* Find Nearest Items */
 	LinkedList<SimpleAgent>	nearestNeighbors(int x,int y);
 	LinkedList<SimpleAgent>	nearestNeighbors(float x,float y);
 	LinkedList<SimpleAgent>	nearestNeighbors(double x,double y);
