dc009e5a39d754e67b203fba10a05da967047929
Seamus McShane <seamus.mcshane@hotmail.com>
2013-11-06 01:46:35 +0000

    Fix bug where view was re-enabled on tab change.

--- src/alifeSim/Gui/SimulationTabPanelManager.java
+++ src/alifeSim/Gui/SimulationTabPanelManager.java
@@ -76,9 +76,6 @@ public class SimulationTabPanelManager extends JTabbedPane implements MouseListe
 							SimulationView.setSim(simulationTabs[i].getSimulation());
 							SimulationView.setSimulationTitle(getTitleAt(getSelectedIndex()));
 							
-							
-							SimulationView.setVisible(true);
-														
 							break;
 						}					
 					}
