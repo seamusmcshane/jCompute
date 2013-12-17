fb4db32c22d28c86949472d7203a15e66b1a6d28
Seamus McShane <seamus.mcshane@hotmail.com>
2013-12-17 12:35:37 +0000

    XML Test harness.

---
+++ src/alifeSim/Scenario/XMLTESTER.java
@@ -0,0 +1,70 @@
+package alifeSim.Scenario;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+
+import org.apache.commons.configuration.ConfigurationException;
+import org.apache.commons.configuration.XMLConfiguration;
+
+import alifeSim.Scenario.SAPP.SAPPScenario;
+
+public class XMLTESTER
+{
+	public static void main(String []args)
+	{
+		System.out.println("XML TESTER");
+		File file = new File("scenarios/default.xml");		
+		
+		XMLConfiguration scenario = new XMLConfiguration();
+
+		scenario.setSchemaValidation(true);
+		SAPPScenario sapp = new SAPPScenario();
+		
+		BufferedReader bufferedReader = null;
+		try
+		{
+			bufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream(file),"ISO_8859_1"));
+			
+		}
+		catch (FileNotFoundException e1)
+		{
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		catch (UnsupportedEncodingException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		String sCurrentLine;
+		String editor = "";
+		try
+		{
+			while ((sCurrentLine = bufferedReader.readLine()) != null)
+			{
+				editor = editor + sCurrentLine;
+				// scenarioEditor.insert(sCurrentLine,
+				// scenarioEditor.getLineCount());
+				// System.out.println(sCurrentLine);
+			}
+		}
+		catch (IOException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println(editor);
+							
+		sapp.loadConfig(editor);
+
+		
+	}
+	
+}
