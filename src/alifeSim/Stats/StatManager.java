09c69eb2462b0bc5edd32faf8a2071ff5f12693f
Seamus McShane <seamus.mcshane@hotmail.com>
2013-12-19 18:35:10 +0000

    Stat Manager can now export XML files.

--- src/alifeSim/Stats/StatManager.java
+++ src/alifeSim/Stats/StatManager.java
@@ -12,6 +12,10 @@ import java.util.concurrent.Semaphore;
 
 import javax.swing.JOptionPane;
 
+import org.apache.commons.lang.StringEscapeUtils;
+
+import com.sun.org.apache.xerces.internal.util.XMLChar;
+
 public class StatManager
 {
 	private String managerName;
@@ -173,7 +177,7 @@ public class StatManager
 			// So we don't spam more than once.
 			boolean progressTrigger = false;
 			
-			// Loop for the lenght of the stat history (sim run length)
+			// Loop for the length of the stat history (sim run length)
 			while(history<historyLength)
 			{
 				// Calculate the %progress
@@ -334,7 +338,7 @@ public class StatManager
 			// So we don't spam more than once.
 			boolean progressTrigger = false;
 			
-			// Loop for the lenght of the stat history (sim run length)
+			// Loop for the length of the stat history (sim run length)
 			while(history<historyLength)
 			{
 				// Calculate the %progress
@@ -393,6 +397,164 @@ public class StatManager
 		
 		statsManagerLock.release();
 	}
+	
+	public void exportStatsToXML(String directory)
+	{
+		statsManagerLock.acquireUninterruptibly();
+		
+		System.out.println("Export Stats to XML");
+		
+		Set<String> groupList = getGroupList();
+		
+		// Each stat group is a new file to output
+		String[] fileNames = new String[groupList.size()];
+		for(String fileName: fileNames)
+		{
+			fileName = new String();
+		}	
+		
+		// For efficient concatenation we use StringBuilder to build up the output file
+		StringBuilder[] fileData = new StringBuilder[groupList.size()];
+		for(int i=0;i<fileData.length;i++)
+		{
+			fileData[i] = new StringBuilder();
+		}
+		
+		System.out.println("Processing Statistic Groups");
+		int groupIndex = 0;
+		
+		// Loop over each stat group
+		for (String group : groupList) 
+		{
+			// Set the File Name
+			fileNames[groupIndex] = group;
+			System.out.println("Group : " + fileNames[groupIndex]);
+			
+			// Get the Stat Group for export
+			StatGroup statGroup = map.get(group);
+			List<String> statList = statGroup.getStatList();
+			
+			// XML HEADER
+			
+			fileData[groupIndex].append("<?xml version=\"1.0\"?>\n");
+						
+			// The boundary's of the stats
+			int statCount = statList.size();
+			int statIndex = 0;
+			
+			// DOCTYPE (DTD)
+			fileData[groupIndex].append("<!DOCTYPE "+ xmlString(group) +"\n[\n");
+
+			// Group contains Steps
+			fileData[groupIndex].append("<!ELEMENT "+ xmlString(group)+" (Step)>\n");
+			
+			// Step Contains Stat Types
+			fileData[groupIndex].append("<!ELEMENT Step (");
+			for(statIndex=0;statIndex< statCount;statIndex++)
+			{
+				fileData[groupIndex].append(xmlString(statList.get(statIndex)));
+				if(statIndex<(statCount-1))
+				{
+					fileData[groupIndex].append(",");
+				}
+			}
+			fileData[groupIndex].append(")>\n");
+			
+			// Each Step has an attribute which is a unique id
+			fileData[groupIndex].append("<!ATTLIST Step id ID #REQUIRED>\n");
+			
+			// Each Stat is an ELEMENT
+			for(statIndex=0;statIndex< statCount;statIndex++)
+			{
+				fileData[groupIndex].append("<!ELEMENT "+ xmlString(statList.get(statIndex))+" (#PCDATA)>\n");
+			}
+				
+			// End DOCTYPE
+			fileData[groupIndex].append("]>\n");
+			
+			// XML ROOT NODE OPEN
+			fileData[groupIndex].append("<" + xmlString(group) + ">\n");			
+			
+			// The Data Rows
+			// Get the history length of the stats (all the same length in steps)
+			int historyLength = statGroup.getStat(statList.get(0)).getHistoryLength();
+			Integer[][] statHistorys = new Integer[statCount][historyLength];
+			
+			// Convert each Linked list to arrays - so we can look up individual indexes quicker later.
+			for(statIndex=0;statIndex< statCount;statIndex++)
+			{
+				statHistorys[statIndex] = statGroup.getStat(statList.get(statIndex)).getHistory().toArray(new Integer[historyLength]);
+			}
+
+			int history =0;
+			System.out.print("DataRows for " + group + " Progress :");
+			
+			// For progress output
+			int percentage = 0;
+			
+			// So we don't spam more than once.
+			boolean progressTrigger = false;
+			
+			// Loop for the length of the stat history (sim run length)
+			while(history<historyLength)
+			{
+				// Calculate the %progress
+				percentage = (int)(( ((float)history) / ((float)historyLength))*100);				
+				
+				// Output every 10%
+				if( ((percentage%10) == 0) )
+				{
+					// Dont spam for each sub interval  i.e. 10.0% to 10.9%
+					if(!progressTrigger)
+					{	
+						System.out.print(" " + percentage);
+						progressTrigger = true;
+					}
+				}
+				else
+				{
+					progressTrigger = false;
+				}
+
+				// The first stat
+				statIndex = 0;
+				
+				
+				// Each Row is a Step
+				fileData[groupIndex].append("\t<Step id='"+history+"'>\n");
+				
+				// Do the same for every history, append , after each sample or a new line after each history
+				for(statIndex=0;statIndex< statCount;statIndex++)
+				{
+					fileData[groupIndex].append("\t\t<"+ xmlString(statList.get(statIndex))+">"+statHistorys[statIndex][history]+"</"+ xmlString(statList.get(statIndex))+">\n");					
+				}
+				
+				// End Step
+				fileData[groupIndex].append("\t</Step>\n");
+				
+				history++;
+			}
+			System.out.print(" 100\n");
+			
+			// XML ROOT NODE CLOSE
+			fileData[groupIndex].append("</" + xmlString(group) + ">\n");
+			
+			groupIndex++;
+		}
+			
+		
+		// Now send the strings to the output writer
+		System.out.println("Writing Files");
+		for(int i=0;i<fileNames.length;i++)
+		{
+			System.out.print(fileNames[i] + " ");			
+			//System.out.print(fileData[i]);
+			writeFiles(directory,fileNames[i],fileData[i].toString(),"xml");
+		}
+		
+		statsManagerLock.release();
+	}
+	
 	
 	/**
 	 * Method checks a string according to XML entity naming rules and returns a corrected string if needed.
