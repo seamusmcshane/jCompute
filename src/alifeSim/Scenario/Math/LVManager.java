fe1632ac12817326ff27ad7aa4880270804e0675
Seamus McShane <seamus.mcshane@hotmail.com>
2014-01-29 19:38:30 +0000

    Added a phase plot as the simulation view.

--- src/alifeSim/Scenario/Math/LVManager.java
+++ src/alifeSim/Scenario/Math/LVManager.java
@@ -1,13 +1,22 @@
 package alifeSim.Scenario.Math;
 
 import java.awt.Color;
+import java.awt.geom.Point2D;
 import java.util.LinkedList;
 import java.util.List;
 
+import javax.swing.text.html.HTMLDocument.Iterator;
+
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.geom.Line;
+import org.newdawn.slick.geom.Point;
+import org.newdawn.slick.geom.Rectangle;
+
+import alifeSim.Alife.GenericPlant.GenericPlant;
 import alifeSim.Stats.SingleStat;
 import alifeSim.Stats.StatInf;
 
-public class LVManager
+public strictfp class LVManager
 {
 	/* Defaults */
 	private double initial_prey_population = 800;
@@ -25,41 +34,61 @@ public class LVManager
 	private double predator_population = initial_predator_population;
 	private SingleStat stat_predator_population;
 
-	private double t = 0;
-	private int sub_steps = 64;
+	private int t = 0;
+	private int max_t = 10000;
+	private int sub_steps = 256;
+	private int draw_mod = 1;
+	
 	private double dt = 1/(double)sub_steps;
+	
+	private List<Point2D> values;
 	
 	public LVManager()
 	{
+		values = new LinkedList<Point2D>();
+		
 		setUpStats();
 		
 		System.out.println("Time"+"\t\t"+"Predators"+"\t\t"+"Prey");
-		System.out.printf("%5.3f\t\t%5.2f\t\t%5.2f\n",t,initial_prey_population,initial_predator_population);
+		System.out.printf("%d\t\t%5.2f\t\t%5.2f\n",t,initial_prey_population,initial_predator_population);
 	}
 
 	public void doStep()
 	{
-		predator_prey_rk4();
-		t++;
-		
-		stat_predator_population.addSample(predator_population);
-		
-		stat_prey_population.addSample(prey_population);
+		if(t<max_t)
+		{
+			//predator_prey_rk4();
+			
+			predator_prey_euler();
+									
+			t++;
+		}
 		
 	}
 	
 	public void predator_prey_euler()
-	{							
+	{				
+		
+		double dx;
+		double dy;
+		
 		for(int i=1;i<sub_steps;i++)
-		{
-			t = i*dt;
-			
+		{			
 			/* Prey */
-			prey_population += calculate_prey(prey_population,predator_population)*dt;
+			dx = calculate_prey(prey_population,predator_population);
 			
 			/* Predator */
-			predator_population += calculate_predator(prey_population,predator_population)*dt;
+			dy = calculate_predator(prey_population,predator_population);
 			
+			prey_population += dx*dt;
+			predator_population += dy*dt;
+			
+			// Stats
+			stat_predator_population.addSample(predator_population);
+			stat_prey_population.addSample(prey_population);
+			
+			// Draw 
+			addDrawVal(i);			
 		}
 	}
 	
@@ -96,9 +125,26 @@ public class LVManager
 			 
 			predator_population		+= (pdk1 + (2.0 * pdk2) + (2.0 * pdk3) + pdk4)*(1.0/6.0)*dt;
 			prey_population			+= (pyk1 + (2.0 * pyk2) + (2.0 * pyk3) + pyk4)*(1.0/6.0)*dt;
-
+			
+			// Stats
+			stat_predator_population.addSample(predator_population);
+			stat_prey_population.addSample(prey_population);
+			
+			// Draw 
+			addDrawVal(i);
+			
 		}
 		
+	}
+	
+	private void addDrawVal(int i)
+	{
+		if(i%draw_mod == 0)
+		{
+			// Draw 
+			values.add(new Point2D.Double(predator_population,prey_population));			
+		}
+
 	}
 	
 	/* LOTKA-VOLTERA - PREY */
@@ -131,6 +177,63 @@ public class LVManager
 		stat.add(stat_prey_population);
 		
 		return stat;
+	}
+	
+	public void drawLV(Graphics g)
+	{
+		g.clear();
+		g.setAntiAlias(true);
+		Point2D previous;
+		
+		float zoom = 1f;
+		float xscale = 4f*zoom;
+		float yscale = 1f*zoom;
+		
+		float ymax = 0f;
+		float xmax = 0f;
+		
+		if(values!=null)
+		{
+			if(values.size() > 0)
+			{
+				previous = values.get(0);
+
+				for (Point2D point : values) 
+				{					
+					g.setLineWidth(1f);
+					
+					g.setColor(new org.newdawn.slick.Color(255,255,255,16));
+										
+					float x = (float)point.getX();
+					float y =(float)point.getY();
+					
+					g.draw(new Line((float)previous.getX()*xscale,(float)previous.getY()*yscale,x*xscale,y*yscale));
+					
+					if(x>xmax)
+					{
+						xmax = x;
+					}
+					
+					if(y>ymax)
+					{
+						ymax = y;
+					}
+
+					previous = point;
+					
+				}
+				
+				g.setAntiAlias(false);
+				
+				g.setLineWidth(1f);
+				g.setColor(new org.newdawn.slick.Color(255,255,255));
+
+				g.draw(new Rectangle(0,0,xmax*xscale+10f,ymax*yscale+10f));
+			
+			}
+		
+		}
+		
 	}
 	
 }
