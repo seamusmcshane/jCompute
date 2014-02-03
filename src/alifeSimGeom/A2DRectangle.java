aa5d8326e135b1af7306eeae81afcabfccbdb438
Seamus McShane <seamus.mcshane@hotmail.com>
2014-02-03 16:37:32 +0000

    Remove gdx rectangle dependancy from A2DRectangle, and implement relevant methods natively.

--- src/alifeSimGeom/A2DRectangle.java
+++ src/alifeSimGeom/A2DRectangle.java
@@ -1,46 +1,79 @@
 package alifeSimGeom;
 
-import com.badlogic.gdx.math.Rectangle;
-
 public class A2DRectangle
 {
-	private Rectangle rectangle;
+	private float x;
+	private float y;
+	private float x2;
+	private float y2;
+	private float width;
+	private float height;
 	
 	public A2DRectangle(float x, float y, float width, float height)
 	{
 		super();
+
+		this.width = width;
+		this.height = height;
 		
-		rectangle = new Rectangle(x,y,width,height);
+		setLocation(x,y);
 	}
-	
+
 	public boolean contains(float x, float y)
 	{
-		return rectangle.contains(x, y);		
+		// outside left wall?
+		if(x<this.x)
+		{
+			return false;
+		}
+		
+		// outside bottom wall
+		if(y<this.y)
+		{
+			return false;
+		}
+		
+		// outside right wall
+		if(x>x2)
+		{
+			return false;
+		}
+		
+		if(y>y2)
+		{
+			return false;
+		}
+		
+		return true;	
 	}
 	
 	public float getX()
 	{
-		return rectangle.getX();
+		return x;
 	}
 	
 	public float getY()
 	{
-		return rectangle.getY();
+		return y;
 	}
 	
 	public float getWidth()
 	{
-		return rectangle.getWidth();
+		return width;
 	}
 	
 	public float getHeight()
 	{
-		return rectangle.getWidth();
+		return height;
 	}
 	
 	public void setLocation(float x,float y)
 	{
-		rectangle.setPosition(x, y);		
+		this.x = x;
+		this.y = y;
+		
+		this.x2 = x+width;
+		this.y2 = y+height;		
 	}
 	
 }
