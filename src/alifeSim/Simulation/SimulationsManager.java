6ed4d52d02a88840df23317f30e7c202f971d265
Seamus McShane <seamus.mcshane@hotmail.com>
2014-02-05 21:44:27 +0000

    Simulations Manager is now used to interact with simulations and control which simulation is displayed in the view if present.

--- src/alifeSim/Simulation/SimulationsManager.java
+++ src/alifeSim/Simulation/SimulationsManager.java
@@ -1,7 +1,14 @@
 package alifeSim.Simulation;
 
 import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Set;
 import java.util.concurrent.Semaphore;
+
+import alifeSim.ChartPanels.StatPanelAbs;
+import alifeSim.Gui.NewSimView;
+import alifeSim.Scenario.ScenarioInf;
+import alifeSim.Stats.StatManager;
 
 public class SimulationsManager
 {
@@ -15,6 +22,9 @@ public class SimulationsManager
 	
 	/* Total Count of Simulations Ran - used for simulation ID */
 	private int simulationNum;
+	
+	/* Active Sims View */
+	private NewSimView simView;
 		
 	public SimulationsManager(int maxSims)
 	{		
@@ -29,7 +39,7 @@ public class SimulationsManager
 		System.out.println("Max Active Sims : " + maxSims);		
 	}
 
-	public int addSimulation()
+	public int addSimulation(SimulationPerformanceStats stats)
 	{
 		int simId = 0;
 		simulationsManagerLock.acquireUninterruptibly();
@@ -39,38 +49,158 @@ public class SimulationsManager
 		simId = simulationNum;
 		
 		// add sim to struct - index on id
-		// simulations.put(simId, simulation);
+		simulations.put(simId, new Simulation(stats));
+		
 		
 		simulationsManagerLock.release();
 		
 		return simId;
 	}
 	
-	public void removeSimulation()
+	public void removeSimulation(int simId)
 	{
 		simulationsManagerLock.acquireUninterruptibly();
 		
 		// remove sim id...
+				
 		
 		simulationsManagerLock.release();
 	}
 	
-	public void startSim()
+	public void startSim(int simId)
 	{
 		simulationsManagerLock.acquireUninterruptibly();
 		
+		Simulation sim = simulations.get(simId);
 		
+		if(sim!=null)
+		{	
+			sim.startSim();
+		}	
 		
 		simulationsManagerLock.release();		
 	}
 	
-	public void pauseSim()
+	public void pauseSim(int simId)
 	{
 		simulationsManagerLock.acquireUninterruptibly();
 		
+		Simulation sim = simulations.get(simId);
 		
+		if(sim!=null)
+		{	
+			sim.pauseSim();
+		}		
 		
 		simulationsManagerLock.release();		
+	}
+
+	public StatManager getStatManager(int simId)
+	{
+		Simulation sim = simulations.get(simId);
+		
+		StatManager statManager = null;
+		
+		if(sim!=null)
+		{			
+			statManager = simulations.get(simId).getSimManager().getStatmanger();	
+		}
+		
+		return statManager;
+	}
+
+	public void setReqSimStepRate(int simId, int stepRate)
+	{
+		Simulation sim = simulations.get(simId);
+		
+		if(sim!=null)
+		{	
+			sim.setReqStepRate(stepRate);
+		}
+		
+	}
+
+	public boolean isSimPaused(int simId)
+	{
+		Simulation sim = simulations.get(simId);
+		
+		if(sim!=null)
+		{	
+			return sim.simPaused();
+		}
+
+		return false;
+	}
+
+	public void unPauseSim(int simId)
+	{
+		Simulation sim = simulations.get(simId);
+		
+		if(sim!=null)
+		{	
+			sim.unPauseSim();
+		}
+		
+	}
+
+	public void destroySimulation(int simId)
+	{
+		Simulation sim = simulations.get(simId);
+		
+		if(sim!=null)
+		{	
+			clearActiveSim();
+			
+			sim.destroySim();
+		}
+		
+	}
+
+	public void createSimScenario(int simId, ScenarioInf simScenario)
+	{
+		Simulation sim = simulations.get(simId);
+		
+		if(sim!=null)
+		{	
+			sim.createSimScenario(simScenario);
+		}
+		
+	}
+
+	public void setSimOutPutCharts(int simId, LinkedList<StatPanelAbs> charts)
+	{
+		Simulation sim = simulations.get(simId);
+		
+		if(sim!=null)
+		{	
+			sim.setOutPutCharts(charts);
+		}
+		
+	}
+	
+	public void setActiveSim(int simId)
+	{
+		Simulation sim = simulations.get(simId);
+		
+		if(simView!=null)
+		{
+			simView.setSim(sim);
+		}
+		
+	}
+
+	public void setSimView(NewSimView simView)
+	{
+		this.simView = simView;
+	}
+	
+	public void clearActiveSim()
+	{
+		if(simView!=null)
+		{
+			simView.setSim(null);
+		}
+		
 	}
 	
 }
