cd984c2faff371c161d2df215974a52682184d6e
Seamus McShane <seamus.mcshane@hotmail.com>
2014-02-10 15:43:25 +0000

    Add handler for index.html and for SimulationsList XML.

--- src/alifeSim/WebInterface/WebInterface.java
+++ src/alifeSim/WebInterface/WebInterface.java
@@ -1,6 +1,9 @@
 package alifeSim.WebInterface;
 
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.ContextHandler;
+import org.eclipse.jetty.server.handler.HandlerList;
+import org.eclipse.jetty.server.handler.ResourceHandler;
 
 import alifeSim.Simulation.SimulationsManager;
 
@@ -15,7 +18,28 @@ public class WebInterface
 		this.simsManager = simsManager;
 		
 		server = new Server(8080);
-		server.setHandler(new TestHandler(simsManager));
+		
+		ResourceHandler resource_handler = new ResourceHandler();
+		
+        resource_handler.setDirectoriesListed(true);
+        resource_handler.setWelcomeFiles(new String[]{ "index.html" });
+        resource_handler.setResourceBase("./WebInterface");
+		
+        HandlerList handlers = new HandlerList();
+        handlers.addHandler(resource_handler);
+        //handlers.setHandlers(new Handler[] { resource_handler, new DefaultHandler() });
+        //server.setHandler(handlers);
+		
+		// SIM LIST
+        ContextHandler context = new ContextHandler();
+        context.setContextPath("/SimulationsList");
+        context.setResourceBase(".");
+        context.setClassLoader(Thread.currentThread().getContextClassLoader());
+        handlers.addHandler(context);
+        
+        
+		server.setHandler(handlers);
+		context.setHandler(new TestHandler(simsManager));
 		
 		try
 		{
