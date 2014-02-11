f7328c1ea3459de4ccba98cee2b4e0495eeb686d
Seamus McShane <seamus.mcshane@hotmail.com>
2014-02-11 14:41:38 +0000

    Renamed SimulationInfoTab to SimulationListTab. Removed SimulationPerformanceOutputInf Removed SimulationPerformanceStats. Added SimulationState object.

--- src/alifeSim/Simulation/SimulationPerformanceStatsOutputInf.java
+++
@@ -1,9 +0,0 @@
-package alifeSim.Simulation;
-
-public interface SimulationPerformanceStatsOutputInf
-{
-	public void setASPS(int averageStepsPerSecond);
-	public void setStepNo(long simulationSteps);
-	public void setTime(long stepTotalTime);
-	public void clearStats();
-}
