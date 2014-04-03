e1bce3b5df4a43bd4c53472b278176e5603921f0
Seamus McShane <seamus.mcshane@hotmail.com>
2014-04-03 14:24:03 +0100

    Add distanceTo method to DistanceFunctions.

--- src/alifeSim/datastruct/knn/DistanceFunctions.java
+++ src/alifeSim/datastruct/knn/DistanceFunctions.java
@@ -1,6 +1,10 @@
 package alifeSim.datastruct.knn;
 
-public class DistanceFunctions
+import org.khelekore.prtree.DistanceCalculator;
+import org.khelekore.prtree.PointND;
+import alifeSim.Alife.AlifeBodyInf;
+
+public class DistanceFunctions implements DistanceCalculator<AlifeBodyInf>
 {
 
 	/** Return the Squared Euclidien Distance in two dimensions */
@@ -10,10 +14,22 @@ public class DistanceFunctions
 	}
 	
 	/** Return the Squared Euclidien Distance in two dimensions */
+	public static double SquaredEuclidienDistance2D(double x1,double y1,double x2,double y2)
+	{
+		return (((x1-x2)*(x1-x2)) + ((y1-y2)*(y1-y2)));		
+	}
+	
+	/** Return the Squared Euclidien Distance in one dimensions */
 	public static double SquaredEuclidienDistance1D(double from,double to)
 	{
 		return (from*to) - (from*to);
 	}
 
-
+	@Override
+	/** Used In Pr-Tree */
+	public double distanceTo(AlifeBodyInf body, PointND point)
+	{
+		return SquaredEuclidienDistance2D(body.getBodyPos().getX(),body.getBodyPos().getY(),point.getOrd(0),point.getOrd(1));
+	}
+	
 }
