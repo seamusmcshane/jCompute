93be1cfd4d378d8dd33cad898b467c8582b6f418
Seamus McShane <seamus.mcshane@hotmail.com>
2014-08-28 22:59:31 +0100

    Change SimulationStateChangedEvent to include stats and endevent string when state changes.

--- src/jCompute/Simulation/Event/SimulationStateChangedEvent.java
+++ src/jCompute/Simulation/Event/SimulationStateChangedEvent.java
@@ -7,12 +7,18 @@ public class SimulationStateChangedEvent
 {
 	private int simId;
 	private SimState state;
+	private long runTime;
+	private String endEvent;
+	private long stepCount;
 	
-	public SimulationStateChangedEvent(int simId, SimState state)
+	public SimulationStateChangedEvent(int simId, SimState state, long runTime, long stepCount , String endEvent)
 	{
-		DebugLogger.output("Simulation " + simId + " State " + state.toString());
+		DebugLogger.output("SimulationStateChangedEvent " + simId + " State " + state.toString());
 		this.simId = simId;
 		this.state = state;
+		this.runTime = runTime;
+		this.stepCount = stepCount;
+		this.endEvent = endEvent;
 	}
 
 	public int getSimId()
@@ -23,6 +29,21 @@ public class SimulationStateChangedEvent
 	public SimState getState()
 	{
 		return state;
+	}
+	
+	public long getRunTime()
+	{
+		return runTime;
+	}
+	
+	public long getStepCount()
+	{
+		return stepCount;
+	}
+	
+	public String getEndEvent()
+	{
+		return endEvent;
 	}
 	
 }
