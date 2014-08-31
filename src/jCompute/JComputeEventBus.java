c18424826314c2b0024f3a91f1ff5b1330bf023c
Seamus McShane <seamus.mcshane@hotmail.com>
2014-09-01 00:59:23 +0100

    Add ThreadFactory class to JComputeEventBus to allow naming internal threads.

--- src/jCompute/JComputeEventBus.java
+++ src/jCompute/JComputeEventBus.java
@@ -1,6 +1,7 @@
 package jCompute;
 
 import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -21,6 +22,28 @@ public class JComputeEventBus
 
 	public static boolean initAsync()
 	{
+		// Simple Thread factory so Event bus threads are named
+		class SimpleNamedThreadFactory implements ThreadFactory 
+		{
+			private String name;
+			private int numThreads;
+			
+			public SimpleNamedThreadFactory(String name)
+			{
+				this.name = name;
+				numThreads = 1;
+			}
+			
+			@Override
+			public Thread newThread(Runnable r) 
+			{
+				Thread thread = new Thread(r);
+				thread.setName(name + " Thread " + numThreads);
+				numThreads++;
+				return thread;
+			}
+		}
+		
 		if(!init)
 		{
 			/*
@@ -29,7 +52,8 @@ public class JComputeEventBus
 			 * A fixed thread pool of 1 ensures the event orders are preserved in order of posting to the bus.
 			 * This makes the event bus - a non-blocking in-order event dispatcher.
 			 */
-			eventBus = new AsyncEventBus(Executors.newFixedThreadPool(1));
+			 
+			eventBus = new AsyncEventBus(Executors.newFixedThreadPool(1, new SimpleNamedThreadFactory("JComputeEventBus")));
 			init = true;
 			
 			log.info("JComputeEventBus Started");
