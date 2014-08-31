ac3c579b1f2b61d39e5bf80d8ed2f6d17f623ea6
Seamus McShane <seamus.mcshane@hotmail.com>
2014-08-31 20:21:09 +0100

    Remove Debug Toggle in DebugLogger to find usage.

--- src/jCompute/Debug/DebugLogger.java
+++ src/jCompute/Debug/DebugLogger.java
@@ -1,38 +1,21 @@
 package jCompute.Debug;
 
 public class DebugLogger
-{
-	private static boolean debug;
-	
-	
-	public static void setDebug(boolean debug)
-	{
-		DebugLogger.debug = debug;
-	}
-		
-	
+{	
 	public static void output(String text)
 	{
-		if(debug)
-		{
-			System.out.println(text);	
-		}
+		System.out.println(text);	
 	}
 	
 	public static void outputString(String text)
 	{
-		if(debug)
-		{
-			System.out.print(text);	
-		}
+		System.out.print(text);	
+
 	}
 	
 	public static void outputChar(char c)
 	{
-		if(debug)
-		{
-			System.out.print(c);	
-		}
+		System.out.print(c);
 	}
 	
 }
