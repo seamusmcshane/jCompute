953ffcf6e7c79a8d8f7cfdbc6c35668c1021b04d
Seamus McShane <seamus.mcshane@hotmail.com>
2014-09-08 19:32:58 +0100

    Remove LotkaVolterraScenario, MandelbrotScenario and AparapiTest.

--- src/jCompute/Scenario/Math/Mandelbrot/Lib/MandelbrotConstants.java
+++
@@ -1,8 +0,0 @@
-package jCompute.Scenario.Math.Mandelbrot.Lib;
-
-public class MandelbrotConstants
-{
-	public static final int PALETTE_SIZE = 256;
-	public static final double MAX_ZOOM = 1.0E15;
-	public static final double DEFAULT_ZOOM = 150;
-}
