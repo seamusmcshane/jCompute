b0dcd2963ffe2d5d22222cdca6bbbfa2403c77c5
Seamus McShane <seamus.mcshane@hotmail.com>
2014-09-29 14:53:01 +0100

    Add launchers for windows x32/x64.

---
+++ launch-grid-node-debug.bat
@@ -0,0 +1,2 @@
+java -Dlog4j.configurationFile="log/config/log4j2-debug.xml" -cp "jCompute.jar;logging/*;logging/log4j2/*" jCompute.Launcher mcs=8,mode=2,addr=127.0.0.1 -server
+
---
+++ launch-standard.bat
@@ -0,0 +1,2 @@
+start javaw -cp "jCompute.jar;logging/*;logging/nop/*" jCompute.Launcher mcs=8,mode=0,iTheme=oxygen,sm=0 -server
+exit
