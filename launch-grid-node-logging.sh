0014261f4baa397f98792ac3d2718caa01e08031
Seamus McShane <seamus.mcshane@hotmail.com>
2014-10-08 18:53:22 +0100

    remove launcher scripts.

--- launch-grid-node-logging.sh
+++
@@ -1,1 +0,0 @@
-java -Dlog4j.configurationFile="log/config/log4j2.xml" -cp "jCompute.jar:logging/*:logging/log4j2/*" jCompute.Launcher mcs=4,mode=2,addr=192.168.254.100 -server
--- launch-standard.bat
+++
@@ -1,2 +0,0 @@
-start javaw -cp "jCompute.jar;logging/*;logging/nop/*" jCompute.Launcher mcs=8,mode=0,iTheme=oxygen,sm=0 -server
-exit
