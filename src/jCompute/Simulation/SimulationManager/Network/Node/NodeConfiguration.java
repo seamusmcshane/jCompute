3d4d4e06f32068a01ae263b108d4059561982c6d
Seamus McShane <seamus.mcshane@hotmail.com>
2014-11-05 20:30:07 +0000

    Store node address and processed simulations in NodeConfiguration.

--- src/jCompute/Simulation/SimulationManager/Network/Node/NodeConfiguration.java
+++ src/jCompute/Simulation/SimulationManager/Network/Node/NodeConfiguration.java
@@ -5,27 +5,29 @@ public class NodeConfiguration
 	private int maxSims = -1;
 	private int uid = -1;
 	private long weighting = Long.MAX_VALUE;
-	
+	private String address = "0.0.0.0";
+	private long simulationsProcessed = 0;
+
 	public void setUid(int uid)
 	{
 		this.uid = uid;
 	}
-	
+
 	public int getUid()
 	{
 		return uid;
 	}
-	
+
 	public void setMaxSims(int maxSims)
 	{
 		this.maxSims = maxSims;
 	}
-	
+
 	public int getMaxSims()
 	{
 		return maxSims;
 	}
-	
+
 	public long getWeighting()
 	{
 		return weighting;
@@ -34,5 +36,25 @@ public class NodeConfiguration
 	public void setWeighting(long weighting)
 	{
 		this.weighting = weighting;
+	}
+
+	public String getAddress()
+	{
+		return address;
+	}
+
+	public void setAddress(String address)
+	{
+		this.address = address;
+	}
+
+	public void incrementSimulationsProcessed()
+	{
+		simulationsProcessed++;
+	}
+
+	public long getSimulationsProcessed()
+	{
+		return simulationsProcessed;
 	}
 }
