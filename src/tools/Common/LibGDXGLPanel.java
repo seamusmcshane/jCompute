ead4ac31176804e6c3dd8e0597286a6c3b3f6fcd
Seamus McShane <seamus.mcshane@hotmail.com>
2015-02-11 17:17:59 +0000

    LibGDX based panel.

---
+++ src/tools/Common/LibGDXGLPanel.java
@@ -0,0 +1,30 @@
+package tools.Common;
+
+import java.awt.BorderLayout;
+
+import javax.swing.JPanel;
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.backends.lwjgl.LwjglCanvas;
+
+public class LibGDXGLPanel extends JPanel
+{
+	private static final long serialVersionUID = 1994385404068550662L;
+
+	public LibGDXGLPanel(ApplicationListener glEnv)
+	{
+		LwjglApplicationConfiguration.disableAudio = true;
+		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
+				
+		cfg.title = "PhasePlot3d";
+		cfg.samples = 8;
+		cfg.vSyncEnabled = true;
+		cfg.useGL20 = true;
+		
+		LwjglCanvas canvas = new LwjglCanvas(glEnv,cfg);		
+		
+		this.setLayout(new BorderLayout());
+		this.add(canvas.getCanvas(), BorderLayout.CENTER);
+		canvas.getGraphics().setVSync(true);
+	}	
+}
