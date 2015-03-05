d6d766b98253f45a1907aeacaf3c40c3dc32d749
Seamus McShane <seamus.mcshane@hotmail.com>
2015-03-05 13:59:16 +0000

    Add Helper method for colouring a mesh (RGB) based on the scale of its values.

--- src/tools/MeshHelper.java
+++ src/tools/MeshHelper.java
@@ -66,4 +66,63 @@ public class MeshHelper
 		
 		return colorVertices;
 	}
+	
+	/**
+	 * Shades all vertices in RGB based on the value of xyz within <min-max>.
+	 * @param inVertices
+	 * @param colorBoost
+	 * @param alpha
+	 * @param xMin
+	 * @param xMax
+	 * @param yMin
+	 * @param yMax
+	 * @param zMin
+	 * @param zMax
+	 * @param scaleHalf
+	 * @return
+	 */
+	public static float[] colorAllVerticesRGBA(float[] inVertices, float colorBoost, float alpha,float xMin, float xMax, float yMin, float yMax, float zMin, float zMax, float scaleHalf)
+	{
+		int indicies = inVertices.length/POS_WIDTH;
+		float[] colorVertices = new float[indicies*(POS_WIDTH+COLOR_WIDTH)];
+		int vLen = inVertices.length;
+
+		if(colorBoost>1f)
+		{
+			colorBoost =1f;
+		}
+		
+		if(colorBoost<0)
+		{
+			colorBoost = 0;
+		}
+		
+		float xColorScale = (1f - colorBoost) / (xMax - xMin);
+		float yColorScale = (1f - colorBoost) / (yMax - yMin);
+		float zColorScale = (1f - colorBoost) / (zMax - zMin);
+
+		float scale = Math.min(xColorScale, yColorScale);
+		scale = Math.min(scale, zColorScale);
+
+		int point = 0;
+		for(int i = 0; i < vLen; i += 3)
+		{
+			colorVertices[point] = inVertices[i];
+			colorVertices[point + 1] = inVertices[i + 1];
+			colorVertices[point + 2] = inVertices[i + 2] + scaleHalf;
+
+			float r = (1f * (scale * inVertices[i])) + colorBoost;
+			float g = (1f * (scale * inVertices[i + 1])) + colorBoost;
+			float b = (1f * (scale * inVertices[i + 2])) + colorBoost;
+
+			colorVertices[point + 3] = r;
+			colorVertices[point + 4] = g;
+			colorVertices[point + 5] = b;
+			colorVertices[point + 6] = alpha;
+
+			point += 7;
+		}
+		
+		return colorVertices;
+	}
 }
