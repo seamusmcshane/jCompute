f405c01648ff4b314968a48d44d76650cc3cbe05
Seamus McShane <seamus.mcshane@hotmail.com>
2015-03-12 16:21:07 +0000

    Color vertices's based on axis order.

--- src/tools/MeshHelper.java
+++ src/tools/MeshHelper.java
@@ -8,7 +8,7 @@ public class MeshHelper
 {
 	private static final int COLOR_WIDTH = 4;
 	private static final int POS_WIDTH = 3;
-	
+
 	/**
 	 * Converts Color Packed Vertices to an indexed mesh
 	 * @param vert
@@ -16,25 +16,28 @@ public class MeshHelper
 	 */
 	public static Mesh verticesToMesh(float[] vert)
 	{
-		/*System.out.println("verticesToMesh vertices " + vert.length);
-		System.out.println("verticesToMesh points " + vert.length/7);
-		System.out.println("verticesToMesh indicies " + (vert.length/7));*/
-		
-		short iLen = (short) (vert.length/(POS_WIDTH+COLOR_WIDTH));
+		/*
+		 * System.out.println("verticesToMesh vertices " + vert.length);
+		 * System.out.println("verticesToMesh points " + vert.length/7);
+		 * System.out.println("verticesToMesh indicies " + (vert.length/7));
+		 */
+
+		short iLen = (short) (vert.length / (POS_WIDTH + COLOR_WIDTH));
 		short[] indicies = new short[iLen];
-		
-		for(short i=0;i<iLen;i++)
+
+		for(short i = 0; i < iLen; i++)
 		{
-			indicies[i]=i;
+			indicies[i] = i;
 		}
-		
-		Mesh mesh = new Mesh(true, vert.length, iLen, new VertexAttribute(Usage.Position, POS_WIDTH, "a_position"),new VertexAttribute(Usage.ColorUnpacked, COLOR_WIDTH, "a_color"));
+
+		Mesh mesh = new Mesh(true, vert.length, iLen, new VertexAttribute(Usage.Position, POS_WIDTH, "a_position"),
+				new VertexAttribute(Usage.ColorUnpacked, COLOR_WIDTH, "a_color"));
 		mesh.setVertices(vert);
 		mesh.setIndices(indicies);
-		
+
 		return mesh;
 	}
-	
+
 	/**
 	 * Shades all vertices the same color
 	 * @param vertices
@@ -43,32 +46,33 @@ public class MeshHelper
 	 */
 	public static float[] colorAllVertices(float[] inVertices, float[] rgba)
 	{
-		int indicies = inVertices.length/POS_WIDTH;
-		
+		int indicies = inVertices.length / POS_WIDTH;
+
 		int vLen = inVertices.length;
-				
-		float[] colorVertices = new float[indicies*(POS_WIDTH+COLOR_WIDTH)];
-		
+
+		float[] colorVertices = new float[indicies * (POS_WIDTH + COLOR_WIDTH)];
+
 		int point = 0;
-		for(int i=0;i<vLen;i+=3)
+		for(int i = 0; i < vLen; i += 3)
 		{
 			colorVertices[point] = inVertices[i];
-			colorVertices[point+1] = inVertices[i+1];
-			colorVertices[point+2] = inVertices[i+2];
-            			
-			colorVertices[point+3] = rgba[0];
-			colorVertices[point+4] = rgba[1];
-			colorVertices[point+5] = rgba[2];
-			colorVertices[point+6] = rgba[3];
-			
-			point+=7;
+			colorVertices[point + 1] = inVertices[i + 1];
+			colorVertices[point + 2] = inVertices[i + 2];
+
+			colorVertices[point + 3] = rgba[0];
+			colorVertices[point + 4] = rgba[1];
+			colorVertices[point + 5] = rgba[2];
+			colorVertices[point + 6] = rgba[3];
+
+			point += 7;
 		}
-		
+
 		return colorVertices;
 	}
-	
+
 	/**
-	 * Shades all vertices in RGB based on the value of xyz within <min-max>.
+	 * Shades all vertices in RGB based on the value of xyz within <min-max> and
+	 * the order of the axis (xyz,yzx,zyx etc..).
 	 * @param inVertices
 	 * @param colorBoost
 	 * @param alpha
@@ -79,30 +83,34 @@ public class MeshHelper
 	 * @param zMin
 	 * @param zMax
 	 * @param scaleHalf
+	 * @param redAxis
+	 * @param greenAxis
+	 * @param blueAxis
 	 * @return
 	 */
-	public static float[] colorAllVerticesRGBA(float[] inVertices, float colorBoost, float alpha,float xMin, float xMax, float yMin, float yMax, float zMin, float zMax, float scaleHalf,int redAxis,int greenAxis,int blueAxis)
+	public static float[] colorAllVerticesRGBA(float[] inVertices, float colorBoost, float alpha, float xMin,
+			float xMax, float yMin, float yMax, float zMin, float zMax, float scaleHalf, int redAxis, int greenAxis,
+			int blueAxis)
 	{
-		int indicies = inVertices.length/POS_WIDTH;
-		float[] colorVertices = new float[indicies*(POS_WIDTH+COLOR_WIDTH)];
+		int indicies = inVertices.length / POS_WIDTH;
+		float[] colorVertices = new float[indicies * (POS_WIDTH + COLOR_WIDTH)];
 		int vLen = inVertices.length;
 
-		if(colorBoost>1f)
+		if(colorBoost > 1f)
 		{
-			colorBoost =1f;
+			colorBoost = 1f;
 		}
-		
-		if(colorBoost<0)
+
+		if(colorBoost < 0)
 		{
 			colorBoost = 0;
 		}
-		
-		float xColorScale = (1f - colorBoost) / (xMax - xMin);
-		float yColorScale = (1f - colorBoost) / (yMax - yMin);
-		float zColorScale = (1f - colorBoost) / (zMax - zMin);
 
-		float scale = Math.min(xColorScale, yColorScale);
-		scale = Math.min(scale, zColorScale);
+		float[] colorsScales = new float[3];
+
+		colorsScales[redAxis] = (1f - colorBoost) / (xMax - xMin);
+		colorsScales[greenAxis] = (1f - colorBoost) / (yMax - yMin);
+		colorsScales[blueAxis] = (1f - colorBoost) / (zMax - zMin);
 
 		int point = 0;
 		for(int i = 0; i < vLen; i += 3)
@@ -111,11 +119,10 @@ public class MeshHelper
 			colorVertices[point + 1] = inVertices[i + 1];
 			colorVertices[point + 2] = inVertices[i + 2] + scaleHalf;
 
-			
 			float[] rgb = new float[3];
-			rgb[0] = (1f * (scale * inVertices[i])) + colorBoost;
-			rgb[1] = (1f * (scale * inVertices[i + 1])) + colorBoost;
-			rgb[2]= (1f * (scale * inVertices[i + 2])) + colorBoost;
+			rgb[0] = (1f * (colorsScales[redAxis] * inVertices[i])) + colorBoost;
+			rgb[1] = (1f * (colorsScales[greenAxis] * inVertices[i + 1])) + colorBoost;
+			rgb[2] = (1f * (colorsScales[blueAxis] * inVertices[i + 2])) + colorBoost;
 
 			colorVertices[point + 3] = rgb[redAxis];
 			colorVertices[point + 4] = rgb[greenAxis];
@@ -124,7 +131,7 @@ public class MeshHelper
 
 			point += 7;
 		}
-		
+
 		return colorVertices;
 	}
 
