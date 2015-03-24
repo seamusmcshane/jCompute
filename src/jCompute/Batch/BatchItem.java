e18624b6431204724ca2ec960272430c75bc5f31
Seamus McShane <seamus.mcshane@hotmail.com>
2015-03-24 15:38:56 +0000

    Remove storage of item configuration text from items - instead a hash is stored.

--- src/jCompute/Batch/BatchItem.java
+++ src/jCompute/Batch/BatchItem.java
@@ -1,16 +1,6 @@
 package jCompute.Batch;
 
-import java.io.BufferedReader;
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
-import java.util.zip.Deflater;
-import java.util.zip.GZIPInputStream;
-import java.util.zip.GZIPOutputStream;
 
 public class BatchItem
 {
@@ -18,7 +8,6 @@ public class BatchItem
 	private int batchId;
 	private int sampleId;
 	private String name;
-	private String compressedText;
 	
 	// position within the combination space of this items combo.
 	private ArrayList<Integer> coordinates;
@@ -32,62 +21,21 @@ public class BatchItem
 	// Finished Info
 	private String endEvent;
 	private long stepCount;
-
+	
 	// Times
 	private long cpuTime;
 	private long netTime;
 	
-	public BatchItem(int sampleId,int itemId,int batchId,String name,String configText,ArrayList<Integer> coordinates,ArrayList<Integer> coordinatesValues)
+	public BatchItem(int sampleId, int itemId, int batchId, String name, String hash, ArrayList<Integer> coordinates,
+			ArrayList<Integer> coordinatesValues)
 	{
 		this.sampleId = sampleId;
 		this.itemId = itemId;
 		this.batchId = batchId;
 		this.name = name;
-		
-		// Compress the string
-        try
-		{
-            ByteArrayOutputStream out = new ByteArrayOutputStream();
-            
-            // This allows continuing to store all batch item configs in memory.
-            GZIPOutputStream gzip = new GZIPOutputStream(out) {
-                {
-                    def.setLevel(Deflater.BEST_COMPRESSION);
-                }
-            };
-            
-			gzip.write(configText.getBytes());
-	        gzip.close();		
-	        compressedText = out.toString("ISO-8859-1");
-		}
-		catch(IOException e1)
-		{
-			e1.printStackTrace();
-		}
-        
+		this.itemHash = hash;
 		this.coordinates = coordinates;
 		this.coordinatesValues = coordinatesValues;
-		
-		String toHash = name+configText;
-		
-		try
-		{
-			byte[] data = MessageDigest.getInstance("MD5").digest(toHash.getBytes());
-			
-	        StringBuilder buffer = new StringBuilder();
-	        for (int d = 0; d < data.length; d++) 
-	        {
-	        	buffer.append(Integer.toString((data[d] & 0xff) + 0x100, 16).substring(1));
-	        }
-	        
-	        itemHash = buffer.toString();
-			
-		}
-		catch (NoSuchAlgorithmException e)
-		{
-			this.itemHash = "MD5 not available";
-		}
-		
 	}
 	
 	public ArrayList<Integer> getCoordinates()
@@ -99,7 +47,7 @@ public class BatchItem
 	{
 		return coordinatesValues;
 	}
-		
+	
 	public int getBatchId()
 	{
 		return batchId;
@@ -113,30 +61,6 @@ public class BatchItem
 	public String getItemName()
 	{
 		return name;
-	}
-	
-	public String getConfigText()
-	{
-		StringBuilder configText = new StringBuilder();
-
-		try
-		{
-			ByteArrayInputStream  byteArrayInputStream = new ByteArrayInputStream(compressedText.getBytes("ISO-8859-1"));
-			GZIPInputStream gzipInputStream = new GZIPInputStream(byteArrayInputStream);
-			BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(gzipInputStream));
-			String line;
-			 
-			while( (line=bufferedReader.readLine()) != null)
-			{
-				configText.append(line);
-			}			  
-		}
-		catch(IOException e)
-		{
-			e.printStackTrace();
-		}
-		
-		return configText.toString();
 	}
 	
 	public int getItemId()
@@ -153,7 +77,7 @@ public class BatchItem
 	{
 		return simId;
 	}
-
+	
 	public int getSampleId()
 	{
 		return sampleId;
@@ -165,7 +89,7 @@ public class BatchItem
 		this.endEvent = endEvent;
 		this.stepCount = stepCount;
 	}
-
+	
 	public void setNetTime(long netTime)
 	{
 		this.netTime = netTime;
@@ -183,14 +107,14 @@ public class BatchItem
 	
 	public long getTotalTime()
 	{
-		return cpuTime+netTime;
+		return cpuTime + netTime;
 	}
 	
 	public String getEndEvent()
 	{
 		return endEvent;
 	}
-
+	
 	public long getStepCount()
 	{
 		return stepCount;
