c7713ef1b8c450003ba9f42c2418397a44b53dfd
Seamus McShane <seamus.mcshane@hotmail.com>
2015-03-24 15:37:49 +0000

    Add file hashing, compression and decompression to FileUtil class.

--- src/jCompute/util/FileUtil.java
+++ src/jCompute/util/FileUtil.java
@@ -1,19 +1,28 @@
 package jCompute.util;
 
-
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FilenameFilter;
+import java.io.IOException;
 import java.net.URISyntaxException;
 import java.net.URL;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.zip.Deflater;
+import java.util.zip.GZIPInputStream;
+import java.util.zip.GZIPOutputStream;
 
 import javax.swing.filechooser.FileFilter;
 
+import org.apache.commons.io.IOUtils;
+
 public class FileUtil
-{	
+{
 	public static File[] getFilesInDir(URL url)
 	{
 		File file = null;
-
+		
 		try
 		{
 			if(url != null)
@@ -25,22 +34,22 @@ public class FileUtil
 		{
 			System.out.println(e.getCause());
 		}
-
+		
 		if(file != null)
 		{
 			return file.listFiles();
 		}
-
+		
 		return new File[]{};
 	}
-
+	
 	public static File[] getFilesInDir(String path)
 	{
 		File file = new File(path);
-
+		
 		return file.listFiles();
 	}
-
+	
 	public static String[] getDirectoriesInDir(String path)
 	{
 		return new File(path).list(new FilenameFilter()
@@ -52,37 +61,37 @@ public class FileUtil
 			}
 		});
 	}
-
+	
 	public static boolean dirContainsFileNamed(String path, String fileName)
 	{
 		File files[] = FileUtil.getFilesInDir(path);
-
+		
 		if(files != null)
 		{
 			for(File file : files)
 			{
-
+				
 				if(file.getName().equals(fileName))
 				{
 					System.out.println(fileName + " Found : " + file.getAbsolutePath());
-
+					
 					return true;
 				}
 			}
 		}
-
+		
 		return false;
 	}
 	
 	public static void createDirIfNotExist(String dir)
 	{
 		File directory = new File(dir);
-
-		if (!directory.exists())
+		
+		if(!directory.exists())
 		{
 			directory.mkdir();
 		}
-
+		
 	}
 	
 	public static FileFilter batchFileFilter()
@@ -93,13 +102,83 @@ public class FileUtil
 			{
 				return f.getName().toLowerCase().endsWith(".batch") || f.isDirectory();
 			}
-
+			
 			@Override
 			public String getDescription()
 			{
 				return "Batch Files";
 			}
 		};
+	}
+	
+	public static byte[] compressBytes(byte[] contents)
+	{
+		byte[] compressedContents = null;
+		
+		// Compress the string
+		try
+		{
+			ByteArrayOutputStream out = new ByteArrayOutputStream();
+			
+			GZIPOutputStream gzip = new GZIPOutputStream(out)
+			{
+				{
+					def.setLevel(Deflater.BEST_COMPRESSION);
+				}
+			};
+			
+			gzip.write(contents);
+			gzip.close();
+			
+			compressedContents = out.toByteArray();
+		}
+		catch(IOException e1)
+		{
+			e1.printStackTrace();
+		}
+		
+		return compressedContents;
+	}
+	
+	public static byte[] decompressBytes(byte[] contentBytes)
+	{
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		try
+		{
+			IOUtils.copy(new GZIPInputStream(new ByteArrayInputStream(contentBytes)), out);
+		}
+		catch(IOException e)
+		{
+			throw new RuntimeException(e);
+		}
+		return out.toByteArray();
+	}
+	
+	public static String HashFile(byte[] contents)
+	{
+		String fileHash = null;
+		
+		try
+		{
+			// Contents hashed as Bytes
+			byte[] byteHash = MessageDigest.getInstance("MD5").digest(contents);
+			
+			// Hash to valid String
+			StringBuilder buffer = new StringBuilder();
+			for(int d = 0; d < byteHash.length; d++)
+			{
+				buffer.append(Integer.toString((byteHash[d] & 0xff) + 0x100, 16).substring(1));
+			}
+			
+			fileHash = buffer.toString();
+		}
+		catch(NoSuchAlgorithmException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return fileHash;
 	}
 	
 	public static FileFilter scenarioFileFilter()
@@ -110,7 +189,7 @@ public class FileUtil
 			{
 				return f.getName().toLowerCase().endsWith(".scenario") || f.isDirectory();
 			}
-
+			
 			@Override
 			public String getDescription()
 			{
@@ -118,5 +197,5 @@ public class FileUtil
 			}
 		};
 	}
-
+	
 }
