db48163bdad7f45cc21a6335b9e1c5808c18d381
Seamus McShane <seamus.mcshane@hotmail.com>
2015-08-25 16:35:07 +0100

    Allow changing tab placement in SimpleTabPanel, allow adding a tab with/without an icon.

--- src/jCompute/Gui/Component/Swing/SimpleTabPanel.java
+++ src/jCompute/Gui/Component/Swing/SimpleTabPanel.java
@@ -2,30 +2,59 @@ package jCompute.Gui.Component.Swing;
 
 import java.awt.BorderLayout;
 import java.awt.Component;
+
+import javax.swing.Icon;
 import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
 
 public class SimpleTabPanel extends JPanel
-{	
+{
 	private static final long serialVersionUID = -2568428138969029838L;
 	
 	private JTabbedPane tabs;
 	
-	public SimpleTabPanel()
+	// Link the SimpleTab Alignment to JTabbedPane Alignments
+	public static final int CENTER = JTabbedPane.CENTER;
+	public static final int TOP = JTabbedPane.TOP;
+	public static final int LEFT = JTabbedPane.LEFT;
+	public static final int BOTTOM = JTabbedPane.BOTTOM;
+	public static final int RIGHT = JTabbedPane.RIGHT;
+	
+	public SimpleTabPanel(int tabPlacement)
 	{
 		// Layout
 		setLayout(new BorderLayout());
 		
-		tabs = new JTabbedPane();
+		tabs = new JTabbedPane(tabPlacement);
 		
-		add(tabs,BorderLayout.CENTER);
-
+		add(tabs, BorderLayout.CENTER);
 	}
 	
+	public SimpleTabPanel()
+	{
+		// Default is Tabs on top
+		this(TOP);
+	}
+	
+	/**
+	 * Add Tab with an Icon
+	 * @param component
+	 * @param icon
+	 * @param name
+	 */
+	public void addTab(Component component, Icon icon, String name)
+	{
+		tabs.addTab(name, icon, component);
+	}
+	
+	/**
+	 * Add Tab with no Icon
+	 * @param component
+	 * @param name
+	 */
 	public void addTab(Component component, String name)
 	{
-		tabs.addTab(name, null,component);
+		tabs.addTab(name, null, component);
 	}
 	
 }
-
