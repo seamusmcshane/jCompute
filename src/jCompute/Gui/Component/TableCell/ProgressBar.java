b0be1e551a4982d69e0e20fe292b0b3ce35ead19
Seamus McShane <seamus.mcshane@hotmail.com>
2016-01-29 00:09:21 +0000

    Fix text vertical alignment in progress bar. Add text anti-aliasing.

--- src/jCompute/Gui/Component/TableCell/ProgressBar.java
+++ src/jCompute/Gui/Component/TableCell/ProgressBar.java
@@ -3,7 +3,9 @@ package jCompute.Gui.Component.TableCell;
 import java.awt.Color;
 import java.awt.FontMetrics;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.Rectangle;
+import java.awt.RenderingHints;
 import java.awt.geom.Rectangle2D;
 import java.text.DecimalFormatSymbols;
 
@@ -13,13 +15,13 @@ import javax.swing.JComponent;
 public class ProgressBar extends JComponent
 {
 	private static final long serialVersionUID = -5090932584537058142L;
-
+	
 	private int progress;
 	private Color fg;
-	private Color bg;	
-	private Color bar;	
+	private Color bg;
+	private Color bar;
 	
-	public ProgressBar() 
+	public ProgressBar()
 	{
 		progress = 0;
 		
@@ -28,7 +30,7 @@ public class ProgressBar extends JComponent
 		bg = Color.LIGHT_GRAY;
 	}
 	
-	public ProgressBar(Color fg, Color bg, Color bar) 
+	public ProgressBar(Color fg, Color bg, Color bar)
 	{
 		progress = 0;
 		
@@ -36,7 +38,7 @@ public class ProgressBar extends JComponent
 		this.bg = bg;
 	}
 	
-	public ProgressBar(int value) 
+	public ProgressBar(int value)
 	{
 		setProgress(value);
 		
@@ -48,7 +50,7 @@ public class ProgressBar extends JComponent
 	{
 		if(value < 100)
 		{
-			progress = value;	
+			progress = value;
 		}
 		else
 		{
@@ -62,37 +64,39 @@ public class ProgressBar extends JComponent
 		{
 			return new DecimalFormatSymbols().getInfinity();
 		}
-
+		
 		return Integer.toString(progress) + " %";
 	}
 	
-	@Override protected void paintComponent(Graphics g)
+	@Override
+	protected void paintComponent(Graphics g1)
 	{
-		super.paintComponent(g);
+		super.paintComponent(g1);
+		
+		Graphics2D g2 = (Graphics2D) g1;
+		g2.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
 		
 		// Draw bar
-		Rectangle clip = g.getClipBounds();
-		g.setColor(bg);
-		g.fillRect(clip.x,clip.y, clip.width,clip.height);
-
-		float barWidth = ((float)progress/100);
+		Rectangle clip = g2.getClipBounds();
+		g2.setColor(bg);
+		g2.fillRect(clip.x, clip.y, clip.width, clip.height);
 		
-		g.setColor(bar);
-		g.fillRect(clip.x,clip.y, (int)(clip.width*barWidth),clip.height);
-
-		FontMetrics fontMetric = g.getFontMetrics(g.getFont());
-		Rectangle2D textSize = fontMetric.getStringBounds(progressToString(), g);
+		float barWidth = ((float) progress / 100);
+		
+		g2.setColor(bar);
+		g2.fillRect(clip.x, clip.y, (int) (clip.width * barWidth), clip.height);
+		
+		FontMetrics fontMetric = g2.getFontMetrics(g2.getFont());
+		Rectangle2D textSize = fontMetric.getStringBounds(progressToString(), g2);
 		
 		// Draw Label
-		g.setColor(fg);				
-		g.drawString(progressToString(), (int) (clip.width-textSize.getWidth())/2, (int) (clip.height - textSize.getHeight()/2) + fontMetric.getDescent());
+		g2.setColor(fg);
+		g2.drawString(progressToString(), (int) (clip.width - textSize.getWidth()) / 2, (int) (clip.height - textSize.getHeight() / 2) + (fontMetric.getDescent() / 2));
 	}
-
+	
 	public void setBG(Color background)
 	{
-		this.bg = background;		
+		this.bg = background;
 	}
-
+	
 }
-
-
