b0bda55e982a20fcc71045501dc1ea854b42f290
Seamus McShane <seamus.mcshane@hotmail.com>
2016-03-05 07:48:37 +0000

    Disable antialiasing.

--- src/jCompute/Gui/View/View.java
+++ src/jCompute/Gui/View/View.java
@@ -79,7 +79,7 @@ public class View implements ApplicationListener, ViewRendererInf
 		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
 		
 		cfg.title = "View";
-		cfg.samples = 2;
+		cfg.samples = 0;
 		cfg.vSyncEnabled = true;
 		cfg.useGL30 = false;
 		
