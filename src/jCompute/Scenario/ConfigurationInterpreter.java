bdb6491830fc3dc9c5a54c4698dde0ea0d5168da
Seamus McShane <seamus.mcshane@hotmail.com>
2016-03-10 17:03:19 +0000

    Add getText() method which updates the string internal representation if the internal XMLConfiguration has been changed. Make getScenarioXMLText private. Add hasSection method to ConfigurationInterpreter. Close stream in loadConfig. Cleanup.

--- src/jCompute/Scenario/ConfigurationInterpreter.java
+++ src/jCompute/Scenario/ConfigurationInterpreter.java
@@ -6,6 +6,7 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -40,7 +41,9 @@ public class ConfigurationInterpreter
 	private XmlSchema schema;
 	private XMLConfiguration configurationFile;
 	private List<StatGroupSetting> statSettingsList;
-	private String configurationFileText;
+	
+	private boolean latchNeedsUpdated;
+	private String latchedFileText;
 	
 	/** Simulation End Events */
 	private HashMap<String, Integer> endEvents;
@@ -50,31 +53,18 @@ public class ConfigurationInterpreter
 		statSettingsList = new ArrayList<StatGroupSetting>();
 	}
 	
-	private void readScenarioEndEvents()
-	{
-		int numEvent = getSubListSize("EndEvents", "Event");
-		
-		endEvents = new HashMap<String, Integer>();
-		
-		String section;
-		
-		for(int i = 0; i < numEvent; i++)
-		{
-			section = "EndEvents.Event(" + i + ")";
-			
-			endEvents.put(getStringValue(section, "Name"), getIntValue(section, "Value"));
-		}
-		
-	}
+	/*
+	 * *****************************************************************************************************
+	 * Loader
+	 *****************************************************************************************************/
 	
-	public void loadConfig(String text)
+	public boolean loadConfig(String text)
 	{
-		this.configurationFileText = text;
-		InputStream stream;
+		boolean status = true;
 		
 		try
 		{
-			stream = new ByteArrayInputStream(configurationFileText.getBytes());
+			InputStream stream = new ByteArrayInputStream(text.getBytes());
 			configurationFile = new XMLConfiguration();
 			configurationFile.setSchemaValidation(true);
 			configurationFile.load(stream);
@@ -82,19 +72,77 @@ public class ConfigurationInterpreter
 			XmlSchemaCollection schemaCol = new XmlSchemaCollection();
 			schema = schemaCol.read(new StreamSource(new FileInputStream((String) configurationFile.getRoot().getAttribute(1).getValue())), null);
 			
-			readScenarioEndEvents();
+			// This is ok to be 0 if the file does not have the section endevents
+			int numEvent = getSubListSize("EndEvents", "Event");
 			
+			endEvents = new HashMap<String, Integer>();
+			
+			String section;
+			
+			for(int i = 0; i < numEvent; i++)
+			{
+				section = "EndEvents.Event(" + i + ")";
+				
+				endEvents.put(getStringValue(section, "Name"), getIntValue(section, "Value"));
+			}
+			
+			stream.close();
+			
+			// Store the text as is
+			latchedFileText = text;
+			latchNeedsUpdated = false;
 		}
 		catch(ConfigurationException e)
 		{
+			status = false;
+			
 			log.error("Error : " + e.toString() + " - " + e.getStackTrace()[0].getMethodName());
 			log.error("Check the file matches its schema");
 		}
 		catch(FileNotFoundException e)
 		{
+			status = false;
+			
 			log.error("Schema File Not Found : " + e.toString() + " - " + e.getStackTrace()[0].getMethodName());
 		}
+		catch(IOException e)
+		{
+			status = false;
+			
+			log.error("Error converting text to stream : " + e.toString() + " - " + e.getStackTrace()[0].getMethodName());
+		}
 		
+		return status;
+	}
+	
+	/*
+	 * *****************************************************************************************************
+	 * Scenario methods
+	 *****************************************************************************************************/
+	
+	public XMLConfiguration scenarioFile()
+	{
+		return configurationFile;
+	}
+	
+	public double getFileVersion()
+	{
+		return Double.parseDouble(configurationFile.getString("Header.Version", "0.00"));
+	}
+	
+	public String getScenarioType()
+	{
+		return configurationFile.getString("Header.Type", "Scenario Type Not Set!!!");
+	}
+	
+	/*
+	 * *****************************************************************************************************
+	 * Section/SubSection Size/Existence methods
+	 *****************************************************************************************************/
+	
+	public int getListSize(String section)
+	{
+		return configurationFile.configurationsAt(section).size();
 	}
 	
 	public int getSubListSize(String section, String value)
@@ -106,6 +154,11 @@ public class ConfigurationInterpreter
 	{
 		return configurationFile.configurationsAt(path).size();
 	}
+	
+	/*
+	 * *****************************************************************************************************
+	 * Value Existence methods
+	 *****************************************************************************************************/
 	
 	public boolean hasStringValue(String section, String value)
 	{
@@ -164,6 +217,11 @@ public class ConfigurationInterpreter
 		
 	}
 	
+	/*
+	 * *****************************************************************************************************
+	 * Value Getter methods
+	 *****************************************************************************************************/
+	
 	public String getStringValue(String section, String value)
 	{
 		return configurationFile.getString(section + "." + value);
@@ -204,21 +262,6 @@ public class ConfigurationInterpreter
 		return configurationFile.getDouble(section + "." + value);
 	}
 	
-	public XMLConfiguration scenarioFile()
-	{
-		return configurationFile;
-	}
-	
-	public double getFileVersion()
-	{
-		return Double.parseDouble(configurationFile.getString("Header.Version", "0.00"));
-	}
-	
-	public String getScenarioType()
-	{
-		return configurationFile.getString("Header.Type", "Scenario Type Not Set!!!");
-	}
-	
 	/**
 	 * Only Sub Class add StatSettings
 	 * @param statSetting
@@ -236,8 +279,8 @@ public class ConfigurationInterpreter
 		for(int i = 0; i < statisticsGroups; i++)
 		{
 			section = "Statistics.Stat(" + i + ")";
-			addStatSettings(new StatGroupSetting(getStringValue(section, "Name"), getBooleanValue(section, "Enabled"), getBooleanValue(section, "TotalStat"),
-					getBooleanValue(section, "Graph"), getIntValue(section, "StatSampleRate"), getIntValue(section, "GraphSampleWindow")));
+			addStatSettings(new StatGroupSetting(getStringValue(section, "Name"), getBooleanValue(section, "Enabled"), getBooleanValue(section, "TotalStat"), getBooleanValue(section, "Graph"),
+					getIntValue(section, "StatSampleRate"), getIntValue(section, "GraphSampleWindow")));
 		}
 		
 		log.debug("Statistics " + statisticsGroups);
@@ -256,68 +299,6 @@ public class ConfigurationInterpreter
 	public int getEventValue(String eventName)
 	{
 		return endEvents.get(eventName);
-	}
-	
-	// Change a value in the XML
-	public void changeValue(String section, String field, Object value)
-	{
-		configurationFile.setProperty(section + "." + field, value);
-	}
-	
-	public String getScenarioXMLText()
-	{
-		ByteArrayOutputStream baos = null;
-		
-		try
-		{
-			baos = new ByteArrayOutputStream();
-			configurationFile.save(baos);
-		}
-		catch(ConfigurationException e)
-		{
-			baos = null;
-			log.error("Error getting scenario XML");
-		}
-		
-		if(baos == null)
-		{
-			return "";
-		}
-		
-		return baos.toString();
-	}
-	
-	public void dumpXML()
-	{
-		ConfigurationNode rootNode = configurationFile.getRoot();
-		
-		String rootName = rootNode.getName();
-		
-		log.debug("Root Element :" + rootName);
-		log.debug("Schema : " + rootNode.getAttribute(1).getValue());
-		
-		Iterator<String> itr = configurationFile.getKeys();
-		
-		while(itr.hasNext())
-		{
-			
-			String field = itr.next();
-			
-			log.debug(rootName + "." + field);
-			log.debug(stripXMLPath(field) + " : ");
-			
-			XmlSchemaType type = findSubNodeDataType(rootName, field);
-			
-			// Need to keep path during iteration AGENT.size WORLD.size
-			
-			if(type != null)
-			{
-				log.debug(type.getQName().getLocalPart());
-				log.debug(" - " + getValueToString(field, type) + "\n");
-			}
-			
-		}
-		log.debug("");
 	}
 	
 	public String getValueToString(String path, XmlSchemaType type)
@@ -359,6 +340,147 @@ public class ConfigurationInterpreter
 		
 		return type.getQName().getLocalPart();
 	}
+	
+	public List<StatGroupSetting> getStats()
+	{
+		return statSettingsList;
+	}
+	
+	public HashMap<String, Integer> getEndEvents()
+	{
+		return endEvents;
+	}
+	
+	public void setStats(List<StatGroupSetting> statSettingsList)
+	{
+		this.statSettingsList = statSettingsList;
+	}
+	
+	public void setEndEvents(HashMap<String, Integer> endEvents)
+	{
+		this.endEvents = endEvents;
+	}
+	
+	public boolean hasSection(String string)
+	{
+		return(getListSize(string) > 0);
+	}
+	
+	/*
+	 * *****************************************************************************************************
+	 * Editing Methods
+	 *****************************************************************************************************/
+	
+	// Change a value in the XML
+	public void changeValue(String section, String field, Object value)
+	{
+		latchNeedsUpdated = true;
+		
+		configurationFile.setProperty(section + "." + field, value);
+	}
+	
+	// Remove a section from the configuration
+	public void removeSection(String string)
+	{
+		latchNeedsUpdated = true;
+		
+		configurationFile.clearTree(string);
+	}
+	
+	/*
+	 * *****************************************************************************************************
+	 * Text Conversion Related methods
+	 *****************************************************************************************************/
+	
+	private void updateLatchText()
+	{
+		if(!latchNeedsUpdated)
+		{
+			return;
+		}
+		
+		// Update the text from the XML file
+		// As it was changed.
+		
+		latchedFileText = getScenarioXMLText();
+		latchNeedsUpdated = false;
+	}
+	
+	// Returns current value of the configuration as a string, updating the string if necessary.
+	public String getText()
+	{
+		// Update the latch text if necessary
+		updateLatchText();
+		
+		return latchedFileText;
+	}
+	
+	// Converts the XMLConfiguration to XML formated text string
+	private String getScenarioXMLText()
+	{
+		ByteArrayOutputStream baos = null;
+		
+		try
+		{
+			baos = new ByteArrayOutputStream();
+			configurationFile.save(baos);
+		}
+		catch(ConfigurationException e)
+		{
+			baos = null;
+			log.error("Error getting scenario XML");
+		}
+		
+		if(baos == null)
+		{
+			return "";
+		}
+		
+		return baos.toString();
+	}
+	
+	/*
+	 * *****************************************************************************************************
+	 * Debug Methods
+	 *****************************************************************************************************/
+	
+	public void dumpXML()
+	{
+		ConfigurationNode rootNode = configurationFile.getRoot();
+		
+		String rootName = rootNode.getName();
+		
+		log.debug("Root Element :" + rootName);
+		log.debug("Schema : " + rootNode.getAttribute(1).getValue());
+		
+		Iterator<String> itr = configurationFile.getKeys();
+		
+		while(itr.hasNext())
+		{
+			
+			String field = itr.next();
+			
+			log.debug(rootName + "." + field);
+			log.debug(stripXMLPath(field) + " : ");
+			
+			XmlSchemaType type = findSubNodeDataType(rootName, field);
+			
+			// Need to keep path during iteration AGENT.size WORLD.size
+			
+			if(type != null)
+			{
+				log.debug(type.getQName().getLocalPart());
+				log.debug(" - " + getValueToString(field, type) + "\n");
+			}
+			
+		}
+		log.debug("");
+	}
+	
+	/*
+	 * *****************************************************************************************************
+	 * Internal XML Processing Methods
+	 *****************************************************************************************************/
 	
 	// If type is null then the target does not exist.
 	private XmlSchemaType findSubNodeDataType(String startPath, String target)
@@ -407,7 +529,7 @@ public class ConfigurationInterpreter
 		return false;
 	}
 	
-	// Iterate over a complex xml type
+	// Iterate over a complex xml type (recursive)
 	private XmlSchemaType transverseComplexType(String target, String path, XmlSchemaElement schemaElement)
 	{
 		XmlSchemaType dataType = null;
@@ -441,30 +563,5 @@ public class ConfigurationInterpreter
 		
 		return dataType;
 		
-	}
-	
-	public List<StatGroupSetting> getStats()
-	{
-		return statSettingsList;
-	}
-	
-	public HashMap<String, Integer> getEndEvents()
-	{
-		return endEvents;
-	}
-	
-	public void setStats(List<StatGroupSetting> statSettingsList)
-	{
-		this.statSettingsList = statSettingsList;
-	}
-	
-	public void setEndEvents(HashMap<String, Integer> endEvents)
-	{
-		this.endEvents = endEvents;
-	}
-	
-	public String getText()
-	{
-		return configurationFileText;
 	}
 }
