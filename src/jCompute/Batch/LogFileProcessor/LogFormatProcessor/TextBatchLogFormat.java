5be14a3aeaade6142c791ee9a72bb6e0dc38e3d8
Seamus McShane <seamus.mcshane@hotmail.com>
2016-03-17 23:12:57 +0000

    TextBatchLogFormat - Fix unchecked null return values.

--- src/jCompute/Batch/LogFileProcessor/LogFormatProcessor/TextBatchLogFormat.java
+++ src/jCompute/Batch/LogFileProcessor/LogFormatProcessor/TextBatchLogFormat.java
@@ -14,103 +14,123 @@ import jCompute.Timing.TimerObj;
 public class TextBatchLogFormat implements LogFormatInf
 {
 	private static Logger log = LoggerFactory.getLogger(TextBatchLogFormat.class);
-	
+
 	private final String logFormat = "TextBatchLogFormat";
-	
+
 	private String logFileName;
 	private String logType;
 	private int samples;
-	
+
 	private String xAxisName;
 	private String yAxisName;
 	private String zAxisName;
-	
+
 	private ArrayList<TextBatchLogItem> logItems;
-	
+
 	private long processingTime;
-	
+
 	public TextBatchLogFormat(String fileName) throws IOException
 	{
 		logItems = new ArrayList<TextBatchLogItem>();
-		
+
 		File file = new File(fileName);
-		
+
 		TimerObj to = new TimerObj();
-		
+
 		BufferedReader inputFile = new BufferedReader(new FileReader(file));
-		
+
 		boolean readingItems = false;
 		boolean finished = false;
-		
+
 		to.startTimer();
-		
+
 		log.info("Reading Header");
-		
+
 		while(!finished)
 		{
 			if(readingItems)
 			{
-				
-				if(inputFile.readLine().equals("[+Items]"))
+
+				String line = inputFile.readLine();
+
+				if(line == null)
+				{
+					throw new IOException();
+				}
+
+				if(line.equals("[+Items]"))
 				{
 					readItems(inputFile);
 				}
-				
+
 				// Items
 				log.info("Finished Reading Items");
-				
+
 				finished = true;
 			}
 			else
 			{
-				if(inputFile.readLine().equals("[+Header]"))
+				String line = inputFile.readLine();
+
+				if(line == null)
+				{
+					throw new IOException();
+				}
+
+				if(line.equals("[+Header]"))
 				{
 					// Header
 					readHeader(inputFile);
-					
+
 					readingItems = true;
-					
+
 					log.info("Reading Items");
 				}
 				else
 				{
 					finished = true;
-					
+
 					String message = "File is not a batch item log file.";
-					
+
 					log.error(message);
-					
+
 					Throwable throwable = new Throwable(message);
-					
+
 					throwable.setStackTrace(Thread.currentThread().getStackTrace());
-					
+
 					throw new IOException(throwable);
 				}
 			}
 		}
-		
+
 		to.stopTimer();
-		
+
 		inputFile.close();
-		
+
 		processingTime = to.getTimeTaken();
 	}
-	
+
 	/*
 	 * *****************************************************************************************************
 	 * Format Processing Methods
 	 *****************************************************************************************************/
-	
+
 	private void readItems(BufferedReader inputFile) throws IOException
 	{
-		boolean finished = false;
-		
-		while(!finished)
+		boolean finishedItems = false;
+
+		while(!finishedItems)
 		{
 			String line = inputFile.readLine();
+
+			if(line == null)
+			{
+				throw new IOException();
+			}
+
 			if(line.equals("[-Items]"))
 			{
-				finished = true;
+				finishedItems = true;
 			}
 			else
 			{
@@ -121,82 +141,151 @@ public class TextBatchLogFormat implements LogFormatInf
 			}
 		}
 	}
-	
+
 	private void readItem(BufferedReader inputFile) throws IOException
 	{
 		TextBatchLogItem item = new TextBatchLogItem();
-		
+
 		// Max Coords to set as item pos/values
 		int maxCoords = 2;
-		
+
 		// Per item Coord Count
 		int coord = 0;
-		
-		for(String line; !(line = inputFile.readLine()).equals("[-Item]");)
+
+		boolean finishedItem = false;
+
+		while(!finishedItem)
 		{
-			if(line.equals("[+Coordinate]"))
+			String line = inputFile.readLine();
+
+			if(line == null)
+			{
+				throw new IOException();
+			}
+
+			if(line.equals("[-Item]"))
+			{
+				finishedItem = true;
+			}
+			else if(line.equals("[+Coordinate]"))
 			{
 				// Increment count of Coords
 				coord++;
-				
+
 				int pos[] = new int[2];
 				double vals[] = new double[2];
-				
+
 				// Read POS 0
-				String cline = inputFile.readLine();
-				String cpos1 = cline.substring(cline.lastIndexOf('=') + 1, cline.length());
-				pos[0] = Integer.parseInt(cpos1);
-				
-				// Read VAL 0
-				cline = inputFile.readLine();
-				String cval1 = cline.substring(cline.lastIndexOf('=') + 1, cline.length());
-				vals[0] = Double.parseDouble(cval1);
-				
-				while(!(cline = inputFile.readLine()).equals("[-Coordinate]"))
+				line = inputFile.readLine();
+
+				if(line == null)
 				{
-					log.info("Coordinate contains unexpect data");
+					throw new IOException();
 				}
-				
-				cline = inputFile.readLine();
-				if(cline.equals("[+Coordinate]"))
+
+				String cpos1 = line.substring(line.lastIndexOf('=') + 1, line.length());
+				pos[0] = Integer.parseInt(cpos1);
+
+				// Read VAL 0
+				line = inputFile.readLine();
+
+				if(line == null)
 				{
-					// Increment count of Coords
-					coord++;
-					
-					// Read POS 1
-					cline = inputFile.readLine();
-					String cpos2 = cline.substring(cline.lastIndexOf('=') + 1, cline.length());
-					pos[1] = Integer.parseInt(cpos2);
-					
-					// Read VAL 0
-					cline = inputFile.readLine();
-					String cval2 = cline.substring(cline.lastIndexOf('=') + 1, cline.length());
-					vals[1] = Double.parseDouble(cval2);
-					
-					while(!(cline = inputFile.readLine()).equals("[-Coordinate]"))
+					throw new IOException();
+				}
+
+				String cval1 = line.substring(line.lastIndexOf('=') + 1, line.length());
+				vals[0] = Double.parseDouble(cval1);
+
+				int lc = 0;
+				do
+				{
+					line = inputFile.readLine();
+
+					if(lc > 0)
 					{
 						log.info("Coordinate contains unexpect data");
 					}
+
+					if(line == null)
+					{
+						throw new IOException();
+					}
+					lc++;
+				}
+				while(!(line.equals("[-Coordinate]")));
+
+				line = inputFile.readLine();
+
+				if(line == null)
+				{
+					throw new IOException();
+				}
+
+				if(line.equals("[+Coordinate]"))
+				{
+					// Increment count of Coords
+					coord++;
+
+					// Read POS 1
+					line = inputFile.readLine();
+
+					if(line == null)
+					{
+						throw new IOException();
+					}
+
+					String cpos2 = line.substring(line.lastIndexOf('=') + 1, line.length());
+					pos[1] = Integer.parseInt(cpos2);
+
+					// Read VAL 0
+					line = inputFile.readLine();
+
+					if(line == null)
+					{
+						throw new IOException();
+					}
+
+					String cval2 = line.substring(line.lastIndexOf('=') + 1, line.length());
+					vals[1] = Double.parseDouble(cval2);
+
+					lc = 0;
+					do
+					{
+						line = inputFile.readLine();
+
+						if(lc > 0)
+						{
+							log.info("Coordinate contains unexpect data");
+						}
+
+						if(line == null)
+						{
+							throw new IOException();
+						}
+						lc++;
+					}
+					while(!(line.equals("[-Coordinate]")));
 				}
 				else
 				{
 					log.info("Error Parsing Coordinates");
 				}
-				
+
 				// Only set the values for the first two coordinates read (Otherwise 3,4 will overwrite them)
 				if(coord <= maxCoords)
 				{
 					item.setCoordsPos(pos);
 					item.setCoordsVals(vals);
 				}
-				
+
 			}
 			else
 			{
 				int delimiterIndex = line.lastIndexOf('=');
 				String field = line.substring(0, delimiterIndex);
 				String val = line.substring(delimiterIndex + 1, line.length());
-				
+
 				switch(field)
 				{
 					case "IID":
@@ -220,21 +309,31 @@ public class TextBatchLogFormat implements LogFormatInf
 				}
 			}
 		}
-		
+
 		log.debug("Coord : " + coord);
-		
+
 		// Reset the coord counted (per item)
 		coord = 0;
 		logItems.add(item);
 	}
-	
+
 	private void readHeader(BufferedReader inputFile) throws IOException
 	{
-		String line = "";
-		while(!(line = inputFile.readLine()).equals("[-Header]"))
+		boolean finishedHeader = false;
+		while(!finishedHeader)
 		{
-			
-			if(line.equals("[+AxisLabels]"))
+			String line = inputFile.readLine();
+
+			if(line == null)
+			{
+				throw new IOException();
+			}
+
+			if(line.equals("[-Header]"))
+			{
+				finishedHeader = true;
+			}
+			else if(line.equals("[+AxisLabels]"))
 			{
 				readAxisLabels(inputFile);
 			}
@@ -243,124 +342,148 @@ public class TextBatchLogFormat implements LogFormatInf
 				int delimiterIndex = line.lastIndexOf('=');
 				String field = line.substring(0, delimiterIndex);
 				String val = line.substring(delimiterIndex + 1, line.length());
-				
+
 				if(field.equals("Name"))
 				{
-					this.logFileName = val;
+					logFileName = val;
 					log.info("LogName :" + logFileName);
 				}
 				else if(field.equals("LogType"))
 				{
-					this.logType = val;
+					logType = val;
 					log.info("LogType :" + logType);
-					
+
 				}
 				else if(field.equals("Samples"))
 				{
-					this.samples = Integer.parseInt(val);
+					samples = Integer.parseInt(val);
 					log.info("Samples :" + samples);
 				}
 			}
 		}
 	}
-	
+
 	private void readAxisLabels(BufferedReader inputFile) throws IOException
 	{
 		int axisCount = 0;
-		boolean finished = false;
-		while(!finished)
+		boolean finishedAxis = false;
+
+		do
 		{
-			String id = inputFile.readLine();
-			
-			if(id.equals("[-AxisLabels]"))
+			// Axis Id
+			String line = inputFile.readLine();
+
+			if(line == null)
 			{
-				finished = true;
+				throw new IOException();
+			}
+
+			if(line.equals("[-AxisLabels]"))
+			{
+				finishedAxis = true;
 			}
 			else
 			{
-				String axis = inputFile.readLine();
-				String axisName = axis.substring(axis.lastIndexOf('=') + 1, axis.length());
-				
+
+				log.info("Axis id : " + line);
+				// Id not used - not split
+
+				// Axis Name
+				line = inputFile.readLine();
+
+				if(line == null)
+				{
+					throw new IOException();
+				}
+
+				line = line.substring(line.lastIndexOf('=') + 1, line.length());
+
 				// X / Y Axis for SurfacePlots
 				if(axisCount == 0)
 				{
-					xAxisName = axisName;
+					xAxisName = line;
 				}
 				else if(axisCount == 1)
 				{
-					yAxisName = axisName;
+					yAxisName = line;
 				}
-				
-				log.info("Axis " + id + " :" + axisName);
-				
+
+				log.info("Axis Name : " + line);
+
 				axisCount++;
 			}
 		}
-		
+		while(!finishedAxis);
+
 		// Choose Plot Source
 		zAxisName = "StepCount";
 	}
-	
+
 	/*
 	 * *****************************************************************************************************
 	 * Log Format
 	 *****************************************************************************************************/
+	@Override
 	public String getLogFormat()
 	{
 		return logFormat;
 	}
-	
+
 	/*
 	 * *****************************************************************************************************
 	 * Axis Names
 	 *****************************************************************************************************/
-	
+
 	@Override
 	public String getXAxisName()
 	{
 		return xAxisName;
 	}
-	
+
 	@Override
 	public String getYAxisName()
 	{
 		return yAxisName;
 	}
-	
+
 	@Override
 	public String getZAxisName()
 	{
 		return zAxisName;
 	}
-	
+
 	/*
 	 * *****************************************************************************************************
 	 * Log Info
 	 *****************************************************************************************************/
+	@Override
 	public String getLogFileName()
 	{
 		return logFileName;
 	}
-	
+
+	@Override
 	public String getLogType()
 	{
 		return logType;
 	}
-	
+
+	@Override
 	public int getSamples()
 	{
 		return samples;
 	}
-	
+
 	/*
 	 * *****************************************************************************************************
 	 * Log Items
 	 *****************************************************************************************************/
+	@Override
 	public ArrayList<TextBatchLogItem> getLogItems()
 	{
 		return logItems;
 	}
-	
+
 	/*
 	 * *****************************************************************************************************
 	 * Processing Metric
