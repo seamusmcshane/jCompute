4a93b1fe605f99ee871a0de51b404c18791e3cde
Seamus McShane <seamus.mcshane@hotmail.com>
2016-03-20 16:01:30 +0000

    Fix Warning in XMLTest util.

--- src/jCompute/Scenario/XMLTESTER.java
+++ src/jCompute/Scenario/XMLTESTER.java
@@ -12,58 +12,59 @@ import org.apache.commons.configuration.XMLConfiguration;
 
 public class XMLTESTER
 {
-	public static void main(String []args)
+	public static void main(String[] args)
 	{
 		System.out.println("XML TESTER");
-		//File file = new File("scenarios/SAPP/batch/BatchRun-ViewRange.xml");		
-		File file = new File("scenarios/HenonMap/default.scenario");		
-		
+		// File file = new File("scenarios/SAPP/batch/BatchRun-ViewRange.xml");
+		File file = new File("scenarios/HenonMap/default.scenario");
+
 		XMLConfiguration scenario = new XMLConfiguration();
 
 		scenario.setSchemaValidation(true);
 		ConfigurationInterpreter sc = new ConfigurationInterpreter();
-		//LotkaVolterraScenario lv = new LotkaVolterraScenario();
-		
-		
+		// LotkaVolterraScenario lv = new LotkaVolterraScenario();
+
 		BufferedReader bufferedReader = null;
 		try
 		{
-			bufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream(file),"ISO_8859_1"));
-			
+			bufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream(file), "ISO_8859_1"));
+
 		}
-		catch (FileNotFoundException e1)
+		catch(FileNotFoundException e1)
 		{
 			e1.printStackTrace();
 		}
-		catch (UnsupportedEncodingException e)
+		catch(UnsupportedEncodingException e)
 		{
 			e.printStackTrace();
 		}
-		String sCurrentLine;
-		String editor = "";
+
+		StringBuilder editor = new StringBuilder();
+		String line = "";
 		try
 		{
-			while ((sCurrentLine = bufferedReader.readLine()) != null)
+			while(line != null)
 			{
-				editor = editor + sCurrentLine;
+				line = bufferedReader.readLine();
+				editor.append(line);
+
 				// scenarioEditor.insert(sCurrentLine,
 				// scenarioEditor.getLineCount());
 				// System.out.println(sCurrentLine);
 			}
 		}
-		catch (IOException e)
+		catch(IOException e)
 		{
 			e.printStackTrace();
 		}
-		System.out.println(editor);
-							
-		sc.loadConfig(editor);
+		System.out.println(editor.toString());
 
-		
-		//sc.getScenarioXMLText();
+		sc.loadConfig(editor.toString());
+
+		// sc.getScenarioXMLText();
 		sc.dumpXML();
-		
+
 		System.exit(0);
 	}
-	
+
 }
