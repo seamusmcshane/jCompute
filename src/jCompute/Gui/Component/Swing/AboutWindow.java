33a7be590ab3d37e43c1eae979e04d026c9b05e7
Seamus McShane <seamus.mcshane@hotmail.com>
2016-03-22 02:11:34 +0000

    Change About and Benchmark windows to extend JDialog..
    Make BenchmarkWindow modal.

--- src/jCompute/Gui/Component/Swing/AboutWindow.java
+++ src/jCompute/Gui/Component/Swing/AboutWindow.java
@@ -1,101 +1,97 @@
 package jCompute.Gui.Component.Swing;
 
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+import java.awt.event.WindowEvent;
+import java.io.IOException;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+import javax.swing.WindowConstants;
+
 import jCompute.util.JComputeInfo;
 import jCompute.util.JVMInfo;
 import jCompute.util.OSInfo;
 import jCompute.util.Text;
 
-import javax.swing.JFrame;
-
-import java.awt.GridLayout;
-
-import javax.swing.JLabel;
-import javax.swing.SwingConstants;
-
-import java.awt.Dialog.ModalExclusionType;
-import java.awt.Dimension;
-import java.awt.event.FocusEvent;
-import java.awt.event.FocusListener;
-import java.awt.event.WindowEvent;
-import java.io.IOException;
-import java.awt.Font;
-import java.util.Timer;
-import java.util.TimerTask;
-
-import javax.swing.JPanel;
-
-import java.awt.BorderLayout;
-import java.awt.FlowLayout;
-
-public class AboutWindow extends JFrame
+public class AboutWindow extends JDialog
 {
 	private final AboutWindow self;
 	private static final long serialVersionUID = -6597372249572030L;
-	
+
 	private Timer updateTimer;
-	
+
 	private JLabel lblUsedVal;
 	private JLabel lblTotalVal;
 	private JLabel lblMaxVal;
-	
+
 	public AboutWindow()
 	{
 		setResizable(false);
 		self = this;
-		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
 		setType(Type.NORMAL);
-		
+
 		setTitle("jCompute");
-		setModalExclusionType(ModalExclusionType.APPLICATION_EXCLUDE);
-		
+		setModalExclusionType(ModalExclusionType.NO_EXCLUDE);
+
 		BorderLayout borderLayout = new BorderLayout();
 		getContentPane().setLayout(borderLayout);
-		
+
 		// Mem Info
 		JPanel memInfoPanel = new JPanel();
 		memInfoPanel.setLayout(new GridLayout(0, 6, 0, 0));
-		
+
 		JLabel lblUsed = new JLabel("Used Memory");
 		lblUsed.setHorizontalAlignment(SwingConstants.CENTER);
 		lblUsed.setFont(lblUsed.getFont().deriveFont(lblUsed.getFont().getStyle() | Font.BOLD));
 		memInfoPanel.add(lblUsed);
-		
+
 		lblUsedVal = new JLabel("");
 		lblUsedVal.setHorizontalAlignment(SwingConstants.CENTER);
 		memInfoPanel.add(lblUsedVal);
-		
+
 		JLabel lblTotal = new JLabel("Total Memory");
 		lblTotal.setHorizontalAlignment(SwingConstants.CENTER);
 		lblTotal.setFont(lblTotal.getFont().deriveFont(lblTotal.getFont().getStyle() | Font.BOLD));
 		memInfoPanel.add(lblTotal);
-		
+
 		lblTotalVal = new JLabel("");
 		lblTotalVal.setHorizontalAlignment(SwingConstants.CENTER);
 		memInfoPanel.add(lblTotalVal);
-		
+
 		JLabel lblMax = new JLabel("Max Memory");
 		lblMax.setHorizontalAlignment(SwingConstants.CENTER);
 		lblMax.setFont(lblMax.getFont().deriveFont(lblMax.getFont().getStyle() | Font.BOLD));
 		memInfoPanel.add(lblMax);
-		
+
 		lblMaxVal = new JLabel("");
 		lblMaxVal.setHorizontalAlignment(SwingConstants.CENTER);
 		memInfoPanel.add(lblMaxVal);
-		
+
 		// Bottom Pad Panel
 		JPanel bottomPad = new JPanel();
 		FlowLayout flowLayout = (FlowLayout) bottomPad.getLayout();
 		flowLayout.setVgap(10);
 		flowLayout.setHgap(10);
 		getContentPane().add(bottomPad, BorderLayout.SOUTH);
-		
+
 		// MemInfo
 		bottomPad.add(memInfoPanel, BorderLayout.NORTH);
-		
+
 		// Grid Panel
 		int numberOfRows = 8;
 		SimpleGridPanel gridPanel = new SimpleGridPanel(numberOfRows);
-		
+
 		// Row and Indexes
 		int buildIndex = gridPanel.addRow("Built Date", "NOT_SET");
 		int jvmIndex = gridPanel.addRow("JVM Name and Version", "NOT_SET");
@@ -105,45 +101,45 @@ public class AboutWindow extends JFrame
 		int phyMemIndex = gridPanel.addRow("Physical Memory", "NOT_SET");
 		int launchIndex = gridPanel.addRow("Launched", "NOT_SET");
 		final int runTimeIndex = gridPanel.addRow("Running Time", "NOT_SET");
-		
+
 		// Set JC Values
 		String[] jcInfo = retrieveJComputeBuildInfo();
 		gridPanel.changeValText(buildIndex, jcInfo[0]);
 		gridPanel.changeValText(launchIndex, jcInfo[1]);
-		
+
 		// Set JVM Values
 		gridPanel.changeValText(jvmIndex, retrieveJVMInfo());
-		
+
 		// Set Values OS
 		String[] osInfo = retrieveOSInfo();
 		gridPanel.changeValText(osIndex, osInfo[0]);
 		gridPanel.changeValText(archIndex, osInfo[1]);
 		gridPanel.changeValText(hwThreadsIndex, osInfo[2]);
 		gridPanel.changeValText(phyMemIndex, osInfo[3]);
-		
+
 		// Text
 		BorderLayout bl_topPanel = new BorderLayout();
 		bl_topPanel.setHgap(10);
 		JPanel topPanel = new JPanel(bl_topPanel);
 		getContentPane().add(topPanel, BorderLayout.NORTH);
-		
+
 		// Left Pad
 		JPanel leftPad = new JPanel(new BorderLayout());
 		// leftPad.setBackground(Color.BLACK);
 		leftPad.setPreferredSize(new Dimension(50, 20));
 		leftPad.setMinimumSize(new Dimension(50, 20));
 		topPanel.add(leftPad, BorderLayout.WEST);
-		
+
 		// Grid
 		topPanel.add(gridPanel, BorderLayout.CENTER);
-		
+
 		JPanel titlePanel = new JPanel();
 		topPanel.add(titlePanel, BorderLayout.NORTH);
 		JLabel lblJcompute = new JLabel("jCompute");
 		titlePanel.add(lblJcompute);
 		lblJcompute.setHorizontalAlignment(SwingConstants.CENTER);
 		lblJcompute.setFont(lblJcompute.getFont().deriveFont(lblJcompute.getFont().getStyle() | Font.BOLD, lblJcompute.getFont().getSize() + 6f));
-		
+
 		// Auto Refresh Data
 		updateTimer = new Timer("About Update Timer");
 		try
@@ -152,14 +148,14 @@ public class AboutWindow extends JFrame
 			{
 				JVMInfo jvmInfo = JVMInfo.getInstance();
 				JComputeInfo jComputeInfo = JComputeInfo.getInstance();
-				
+
 				@Override
 				public void run()
 				{
 					lblUsedVal.setText(String.valueOf(jvmInfo.getUsedJVMMemory()));
 					lblTotalVal.setText(String.valueOf(jvmInfo.getTotalJVMMemory()));
 					lblMaxVal.setText(String.valueOf(jvmInfo.getMaxMemory()));
-					
+
 					gridPanel.changeValText(runTimeIndex, Text.longTimeToDHMS(jComputeInfo.getRuntime()));
 				}
 			}, 0, 1000);
@@ -168,81 +164,80 @@ public class AboutWindow extends JFrame
 		{
 			e.printStackTrace();
 		}
-		
+
 		// Close on loose focus
 		addFocusListener(new FocusListener()
 		{
-			
+
 			@Override
 			public void focusGained(FocusEvent arg0)
 			{
-			
+
 			}
-			
+
 			@Override
 			public void focusLost(FocusEvent arg0)
 			{
 				self.dispatchEvent(new WindowEvent(self, WindowEvent.WINDOW_CLOSING));
 			}
-			
+
 		});
-		
-		this.pack();
+
+		pack();
 	}
-	
+
 	private String[] retrieveJComputeBuildInfo()
 	{
 		String val[] =
 		{
 			"Error Retrieving Build Information", "Error Retrieving Build Information"
 		};
-		
+
 		try
 		{
 			JComputeInfo info = JComputeInfo.getInstance();
-			
+
 			val[0] = info.getBuildDate();
 			val[1] = info.getLaunched();
 		}
 		catch(IOException e)
 		{
 			e.printStackTrace();
-			
+
 			MessageBox.popup("Error with JComputeInfo", this);
 		}
-		
+
 		return val;
 	}
-	
+
 	private String retrieveJVMInfo()
 	{
 		JVMInfo jvmInfo = JVMInfo.getInstance();
-		
+
 		return jvmInfo.getJVMName() + " " + jvmInfo.getJVMVersion();
 	}
-	
+
 	private String[] retrieveOSInfo()
 	{
 		OSInfo osInfo = OSInfo.getInstance();
-		
+
 		return new String[]
 		{
-			osInfo.getOSName(), osInfo.getSystemArch(), Integer.toString(osInfo.getHWThreads()), Integer.toString(osInfo
-			.getSystemPhysicalMemorySize())
+			osInfo.getOSName(), osInfo.getSystemArch(), Integer.toString(osInfo.getHWThreads()), Integer.toString(osInfo.getSystemPhysicalMemorySize())
 		};
 	}
-	
+
 	@Override
 	public void dispose()
 	{
 		super.dispose();
-		
+
 		if(updateTimer != null)
 		{
 			updateTimer.cancel();
 		}
-		
+
 		updateTimer = null;
 	}
-	
+
 }
--- src/jCompute/Gui/Component/Swing/BenchmarkWindow.java
+++ src/jCompute/Gui/Component/Swing/BenchmarkWindow.java
@@ -1,9 +1,22 @@
 package jCompute.Gui.Component.Swing;
 
 import java.awt.Dimension;
-import java.awt.Dialog.ModalExclusionType;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
 
-import javax.swing.JFrame;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+import javax.swing.WindowConstants;
 
 import com.DaveKoelle.AlphanumFileNameComparator;
 import com.google.common.io.Files;
@@ -11,67 +24,52 @@ import com.google.common.io.Files;
 import jCompute.Cluster.BatchManager.BatchManager;
 import jCompute.util.FileUtil;
 
-import java.io.File;
-import java.util.ArrayList;
-import java.util.Arrays;
-
-import javax.swing.JLabel;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.WindowEvent;
-
-import javax.swing.JButton;
-import javax.swing.JComboBox;
-import javax.swing.JPanel;
-import java.awt.GridLayout;
-import java.awt.Font;
-import javax.swing.SwingConstants;
-
-public class BenchmarkWindow extends JFrame
+public class BenchmarkWindow extends JDialog
 {
 	private static final long serialVersionUID = -6582518152127005845L;
-	
+
 	private final String path = "benchmark";
 	private ArrayList<String> list;
 	private int fileCount;
-	
+
 	public static void main(String args[])
 	{
 		BenchmarkWindow test = new BenchmarkWindow(null);
-		
+
 		test.pack();
 		test.setLocationRelativeTo(null);
 		test.setVisible(true);
 	}
-	
+
 	public BenchmarkWindow(BatchManager batchManager)
 	{
 		BenchmarkWindow self = this;
-		
+
+		setModal(true);
 		setResizable(false);
-		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
 		setType(Type.NORMAL);
-		
+
 		setTitle("jCompute");
 		setMinimumSize(new Dimension(600, 150));
-		setModalExclusionType(ModalExclusionType.APPLICATION_EXCLUDE);
-		
+		setModalExclusionType(ModalExclusionType.NO_EXCLUDE);
+
 		File[] files = FileUtil.getFilesInDir(path);
-		
+
 		// Sort Files Alpha Numerically by FileName
 		Arrays.sort(files, new AlphanumFileNameComparator());
-		
+
 		fileCount = files.length;
-		
+
 		list = new ArrayList<String>();
-		
+
 		getContentPane().setLayout(new GridLayout((fileCount * 2) + 2, 0, 0, 0));
-		
+
 		addPadPanel((JPanel) getContentPane());
-		
+
 		addTitlePanel(this, batchManager);
 		addPadPanel((JPanel) getContentPane());
-		
+
 		for(File file : files)
 		{
 			if(Files.getFileExtension(file.getName()).equals("batch"))
@@ -79,7 +77,7 @@ public class BenchmarkWindow extends JFrame
 				addTestOption(benchmarkStringTrim(file.getName()));
 			}
 		}
-		
+
 		JButton start = new JButton("Start");
 		start.addActionListener(new ActionListener()
 		{
@@ -87,11 +85,11 @@ public class BenchmarkWindow extends JFrame
 			public void actionPerformed(ActionEvent e)
 			{
 				JComputeProgressMonitor openBatchProgressMonitor = new JComputeProgressMonitor(getContentPane(), "Adding Benchmark Batches", 0, 100);
-				
+
 				int size = list.size();
-				
+
 				File[] fileList = new File[size];
-				
+
 				if(size > 0)
 				{
 					for(int f = 0; f < size; f++)
@@ -100,30 +98,31 @@ public class BenchmarkWindow extends JFrame
 						{
 							String name = list.get(f);
 							String fileName = benchmarkStringTrim(file.getName());
-							
+
 							if(name.equals(fileName))
 							{
 								fileList[f] = file;
 							}
 						}
 					}
-					
+
 					self.dispatchEvent(new WindowEvent(self, WindowEvent.WINDOW_CLOSING));
-					
+
 					if(batchManager != null)
 					{
-						OpenBatchFileTask openBatchProgressMonitorTask = new OpenBatchFileTask(getContentPane(), openBatchProgressMonitor, batchManager, fileList);
+						OpenBatchFileTask openBatchProgressMonitorTask = new OpenBatchFileTask(getContentPane(), openBatchProgressMonitor, batchManager,
+						fileList);
 						openBatchProgressMonitorTask.start();
 					}
 				}
 			}
 		});
-		
+
 		addPadPanel((JPanel) getContentPane());
 		JPanel container = new JPanel();
 		getContentPane().add(container);
 		container.setLayout(new GridLayout(0, 7, 0, 0));
-		
+
 		addPadPanel(container);
 		addPadPanel(container);
 		addPadPanel(container);
@@ -132,29 +131,29 @@ public class BenchmarkWindow extends JFrame
 		container.add(start);
 		addPadPanel(container);
 		getContentPane().add(container);
-		
+
 	}
-	
+
 	private void addTitlePanel(BenchmarkWindow benchmarkWindow, BatchManager batchManager)
 	{
 		JPanel container = new JPanel();
 		getContentPane().add(container);
 		container.setLayout(new GridLayout(0, 7, 0, 0));
-		
+
 		addPadPanel(container);
-		
+
 		JLabel label = new JLabel("Test Name");
 		label.setHorizontalAlignment(SwingConstants.CENTER);
 		label.setFont(label.getFont().deriveFont(label.getFont().getStyle() | Font.BOLD));
 		container.add(label);
-		
+
 		addPadPanel(container);
-		
+
 		JLabel label_2 = new JLabel("Test Count");
 		label_2.setHorizontalAlignment(SwingConstants.CENTER);
 		label_2.setFont(label_2.getFont().deriveFont(label_2.getFont().getStyle() | Font.BOLD));
 		container.add(label_2);
-		
+
 		addPadPanel(container);
 		JButton buttton = new JButton("Reset");
 		buttton.addActionListener(new ActionListener()
@@ -166,38 +165,38 @@ public class BenchmarkWindow extends JFrame
 				reset.pack();
 				reset.setLocationRelativeTo(benchmarkWindow);
 				reset.setVisible(true);
-				
+
 				benchmarkWindow.dispatchEvent(new WindowEvent(benchmarkWindow, WindowEvent.WINDOW_CLOSING));
 			}
 		});
 		container.add(buttton);
-		
+
 		addPadPanel(container);
-		
+
 	}
-	
+
 	private void addPadPanel(JPanel container)
 	{
 		JLabel pad1 = new JLabel("");
 		pad1.setHorizontalAlignment(SwingConstants.CENTER);
 		container.add(pad1);
 	}
-	
+
 	private void addTestOption(String name)
 	{
 		JPanel container = new JPanel();
 		getContentPane().add(container);
 		container.setLayout(new GridLayout(0, 7, 0, 0));
-		
+
 		addPadPanel(container);
-		
+
 		JLabel label = new JLabel(name);
 		label.setHorizontalAlignment(SwingConstants.CENTER);
 		label.setFont(label.getFont().deriveFont(label.getFont().getStyle() | Font.BOLD));
 		container.add(label);
-		
+
 		addPadPanel(container);
-		
+
 		JComboBox<Integer> comboBox = new JComboBox<Integer>();
 		comboBox.addItem(0);
 		comboBox.addItem(1);
@@ -209,11 +208,11 @@ public class BenchmarkWindow extends JFrame
 		comboBox.addItem(20);
 		comboBox.addItem(50);
 		container.add(comboBox);
-		
+
 		comboBox.setSelectedIndex(4);
-		
+
 		addPadPanel(container);
-		
+
 		JButton buttton = new JButton("Add");
 		buttton.addActionListener(new ActionListener()
 		{
@@ -225,14 +224,14 @@ public class BenchmarkWindow extends JFrame
 				{
 					list.add(name);
 				}
-				
+
 				comboBox.setEnabled(false);
 				buttton.setEnabled(false);
 			}
 		});
 		container.add(buttton);
 	}
-	
+
 	private String benchmarkStringTrim(String batchName)
 	{
 		return new String(batchName.substring(batchName.indexOf('-') + 1, batchName.lastIndexOf('.')));
