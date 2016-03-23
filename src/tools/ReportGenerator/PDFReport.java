6388b255398db32b50ade2f17339f21cc70308d0
Seamus McShane <seamus.mcshane@hotmail.com>
2016-03-23 10:41:13 +0000

    Update PFDReport to use PDFBox-2.0.0.

--- src/tools/ReportGenerator/PDFReport.java
+++ src/tools/ReportGenerator/PDFReport.java
@@ -8,15 +8,14 @@ import java.util.Map;
 
 import javax.imageio.ImageIO;
 
-import org.apache.pdfbox.exceptions.COSVisitorException;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.PDPageContentStream;
 import org.apache.pdfbox.pdmodel.common.PDRectangle;
-import org.apache.pdfbox.pdmodel.edit.PDPageContentStream;
 import org.apache.pdfbox.pdmodel.font.PDFont;
 import org.apache.pdfbox.pdmodel.font.PDType1Font;
-import org.apache.pdfbox.pdmodel.graphics.xobject.PDPixelMap;
-import org.apache.pdfbox.pdmodel.graphics.xobject.PDXObjectImage;
+import org.apache.pdfbox.pdmodel.graphics.image.LosslessFactory;
+import org.apache.pdfbox.pdmodel.graphics.image.PDImageXObject;
 
 public class PDFReport
 {
@@ -36,7 +35,8 @@ public class PDFReport
 	private int imageWidth;
 	private int imageHeight;
 	
-	public PDFReport(String reportFileName, ArrayList<String> rowNames, ArrayList<String> colNames, Map<String, String> cells, String fullPath, float scale, int imageWidth, int imageHeight)
+	public PDFReport(String reportFileName, ArrayList<String> rowNames, ArrayList<String> colNames, Map<String, String> cells, String fullPath, float scale,
+	int imageWidth, int imageHeight)
 	{
 		super();
 		this.reportFileName = reportFileName;
@@ -53,25 +53,19 @@ public class PDFReport
 	
 	public void generate() throws IOException
 	{
-		try
-		{
-			PDDocument document = new PDDocument();
-			
-			addReportPage(reportFileName, document, rowNames, colNames, fullPath, scale, "Averages");
-			addReportPage(reportFileName, document, rowNames, colNames, fullPath, scale, "Standard Deviations");
-			addReportPage(reportFileName, document, rowNames, colNames, fullPath, scale, "Max");
-			
-			document.save(fullPath + File.separator + reportFileName + ".pdf");
-			
-			document.close();
-		}
-		catch(COSVisitorException e)
-		{
-			throw new IOException(e);
-		}
+		PDDocument document = new PDDocument();
+		
+		addReportPage(reportFileName, document, rowNames, colNames, fullPath, scale, "Averages");
+		addReportPage(reportFileName, document, rowNames, colNames, fullPath, scale, "Standard Deviations");
+		addReportPage(reportFileName, document, rowNames, colNames, fullPath, scale, "Max");
+		
+		document.save(fullPath + File.separator + reportFileName + ".pdf");
+		
+		document.close();
 	}
 	
-	private void addReportPage(String reportFileName, PDDocument doc, ArrayList<String> rowNames, ArrayList<String> colNames, String fullPath, float scale, String pageTitle) throws IOException
+	private void addReportPage(String reportFileName, PDDocument doc, ArrayList<String> rowNames, ArrayList<String> colNames, String fullPath, float scale,
+	String pageTitle) throws IOException
 	{
 		int documentTitleSize = 32;
 		int pageTitleSize = 18;
@@ -136,12 +130,13 @@ public class PDFReport
 				System.out.println(imagePath);
 				
 				addImage(doc, page, cos, imagePath, imageWidth * colX + xMargin, imageHeight * rowY + yMargin + titleHeight, scale);
-				addRectangle(doc, page, cos, imageWidth * colX + xMargin, imageHeight * rowY + yMargin + titleHeight, imageWidth * scale, imageHeight * scale, 2f * scale);
+				addRectangle(doc, page, cos, imageWidth * colX + xMargin, imageHeight * rowY + yMargin + titleHeight, imageWidth * scale, imageHeight * scale,
+				2f * scale);
 				
 				cos.beginText();
 				cos.setFont(PDType1Font.HELVETICA, 12);
-				cos.moveTextPositionByAmount(10 + xMargin, pageBox.getHeight() - ((imageHeight * rowY) + 24 + yMargin + titleHeight));
-				cos.drawString(row);
+				cos.newLineAtOffset(10 + xMargin, pageBox.getHeight() - ((imageHeight * rowY) + 24 + yMargin + titleHeight));
+				cos.showText(row);
 				cos.endText();
 				
 				colX++;
@@ -170,7 +165,8 @@ public class PDFReport
 	 * @return
 	 * @throws IOException
 	 */
-	private float[] addText(float xOffset, float yOffset, String text, PDDocument doc, PDPage page, PDPageContentStream cs, PDFont font, float size) throws IOException
+	private float[] addText(float xOffset, float yOffset, String text, PDDocument doc, PDPage page, PDPageContentStream cs, PDFont font, float size)
+	throws IOException
 	{
 		cs.beginText();
 		
@@ -183,8 +179,8 @@ public class PDFReport
 		System.out.println("center " + center);
 		System.out.println("String " + stringWidth);
 		
-		cs.moveTextPositionByAmount((center - stringWidth) + xOffset, (page.getTrimBox().getHeight() - fontHeight) - yOffset);
-		cs.drawString(text);
+		cs.newLineAtOffset((center - stringWidth) + xOffset, (page.getTrimBox().getHeight() - fontHeight) - yOffset);
+		cs.showText(text);
 		
 		cs.endText();
 		
@@ -198,15 +194,16 @@ public class PDFReport
 	{
 		BufferedImage awtImage = ImageIO.read(new File(file));
 		
-		PDXObjectImage ximage = new PDPixelMap(doc, awtImage);
+		PDImageXObject ximage = LosslessFactory.createFromImage(doc, awtImage);
 		
 		float width = ximage.getWidth() * scale;
 		float height = ximage.getHeight() * scale;
 		
-		cs.drawXObject(ximage, x, -y + page.getMediaBox().getHeight() - height, width, height);
+		cs.drawImage(ximage, x, -y + page.getMediaBox().getHeight() - height, width, height);
 	}
 	
-	private void addRectangle(PDDocument doc, PDPage page, PDPageContentStream cs, float x, float y, float width, float height, float lineWidth) throws IOException
+	private void addRectangle(PDDocument doc, PDPage page, PDPageContentStream cs, float x, float y, float width, float height, float lineWidth)
+	throws IOException
 	{
 		cs.setLineWidth(lineWidth);
 		cs.addRect(x, -y + page.getMediaBox().getHeight() - height, width, height);
