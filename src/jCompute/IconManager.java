959398cd43fe1a4cb19ca70192362090454e792f
Seamus McShane <seamus.mcshane@hotmail.com>
2016-03-29 15:16:47 +0100

    Rework IconManager to use a mapping file (text).
     Mapping file format -:
     Lines starting with # are comments and ignored.
     Lines that start with a space will trigger a log warning.
     Mappings declarations are comma separated name,relative path pairs.
     Mappings that have a relative path starting with a ! are cross reference mappings.
     Cross reference mappings declarations - point to an existing mapping in the format name,!existingMappingName.
     Cross reference mappings declarations can be in any location in the mapping file, including before the exiting mapping declaration.

--- src/jCompute/IconManager.java
+++ src/jCompute/IconManager.java
@@ -1,68 +1,209 @@
 package jCompute;
 
-import jCompute.util.FileUtil;
-
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.HashMap;
+import java.util.LinkedList;
 
 import javax.swing.ImageIcon;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-public class IconManager
+public final class IconManager
 {
 	// Log4j2 Logger
 	private static Logger log = LogManager.getLogger(IconManager.class);
 	
 	private static HashMap<String, ImageIcon> iconMap;
 	
-	private static String smallIconDirName = "16x16";
-	private static String mediumIconDirName = "32x32";
-	
 	@SuppressWarnings("unused")
 	private static IconManager iconManager;
+	
+	private IconManager(String themeName)
+	{
+		if(themeName.equals("none"))
+		{
+			return;
+		}
+		
+		try
+		{
+			iconMap = new HashMap<String, ImageIcon>();
+			
+			log.info("Loading icon theme " + themeName);
+			
+			String themeURI = "/icons/" + themeName + "/";
+			
+			readIconsViaMapping(themeURI);
+		}
+		catch(IOException | URISyntaxException e)
+		{
+			log.warn("Cannot read mapping file for " + themeName);
+		}
+	}
 	
 	public static void init(String themeName)
 	{
 		iconManager = new IconManager(themeName);
 	}
 	
-	public static ImageIcon getIcon(String iconName)
+	public static ImageIcon retrieveIcon(String iconName)
 	{
-		return iconMap.get(iconName + ".png");
-	}
-	
-	private IconManager(String themeName)
-	{
-		iconMap = new HashMap<String, ImageIcon>();
-		
-		log.info("Loading icon theme " + themeName);
-		
-		String themePath = "/icons/" + themeName + "/";
-		
-		URL smallIconURL;
-		URL mediumIconURL;
-		
-		smallIconURL = IconManager.class.getResource(themePath + smallIconDirName);
-		mediumIconURL = IconManager.class.getResource(themePath + mediumIconDirName);
-		
-		loadIcons(FileUtil.getFilesInDir(smallIconURL));
-		loadIcons(FileUtil.getFilesInDir(mediumIconURL));
-	}
-	
-	private void loadIcons(File[] files)
-	{
-		for(int f = 0; f < files.length; f++)
+		if(iconMap == null)
 		{
-			log.debug("File : " + files[f].getName() + " " + files[f].getAbsolutePath());
+			return null;
+		}
+		
+		ImageIcon icon = iconMap.get(iconName);
+		
+		if(icon == null)
+		{
+			log.warn("No matching icon found for " + iconName);
 			
-			if(files[f].isFile())
+			Thread.dumpStack();
+		}
+		
+		return icon;
+	}
+	
+	/**
+	 * Mapping file format
+	 * Purpose to map original icon names to jCompute internal names and avoid needing to rename icons.
+	 * Lines starting with # are comments and ignored.
+	 * Lines that start with a space will trigger a log warning.
+	 * Mappings declarations are comma separated name,relative path pairs
+	 * Mappings that have a relative path starting with a ! are cross reference mappings.
+	 * Cross reference mappings declarations - point to an existing mapping in the format name,!existingMappingName.
+	 * Cross reference mappings declarations can be in any location in the mapping file, including before the exiting mapping declaration.
+	 *
+	 * @param themeURI
+	 * @throws IOException
+	 * @throws URISyntaxException
+	 */
+	private void readIconsViaMapping(String themeURI) throws IOException, URISyntaxException
+	{
+		LinkedList<String[]> crossRefs = new LinkedList<String[]>();
+		
+		// Locate the theme icons path. (Note URL)
+		URL baseURL = IconManager.class.getResource(themeURI);
+		
+		// Find the full path to the mapping file (URLtoPath via URI)
+		String basePath = new File(baseURL.toURI()).getAbsolutePath();
+		String mappingFilePath = basePath + File.separator + "icon.mapping";
+		
+		log.info(mappingFilePath);
+		
+		BufferedReader reader = new BufferedReader(new FileReader(new File(mappingFilePath)));
+		
+		String line = reader.readLine();
+		
+		int lineNo = 0;
+		
+		while(line != null)
+		{
+			boolean skipLine = false;
+			
+			if(line.length() == 0)
 			{
-				iconMap.put(files[f].getName(), new ImageIcon(files[f].getAbsolutePath(), files[f].getName()));
+				log.warn("Got empty line in mapping file at " + lineNo);
+				
+				skipLine = true;
 			}
+			
+			if(!skipLine)
+			{
+				if(!(line.charAt(0) == '#'))
+				{
+					boolean error = false;
+					try
+					{
+						
+						// Read the line
+						String jcName = line.substring(0, line.indexOf(','));
+						String path = line.substring(line.indexOf(',') + 1, line.length());
+						
+						if((jcName == null) || (path == null))
+						{
+							error = true;
+						}
+						else
+						{
+							if(path.charAt(0) == '!')
+							{
+								String ref = path.substring(1, path.length());
+								
+								// Path in this case is a crossref
+								crossRefs.add(new String[]
+								{
+									jcName, ref
+								});
+							}
+							else
+							{
+								addIcon(jcName, basePath + File.separator + path);
+							}
+						}
+					}
+					catch(StringIndexOutOfBoundsException e)
+					{
+						error = true;
+					}
+					
+					if(error)
+					{
+						log.error("Problem reading mapping in file at Line " + lineNo);
+					}
+				}
+				else
+				{
+					// a line Comment
+					// # .....
+				}
+				
+			}
+			
+			lineNo++;
+			line = reader.readLine();
+		}
+		
+		// Map the cross refs.
+		for(String[] crossRef : crossRefs)
+		{
+			addCrossRef(crossRef[0], crossRef[1]);
+		}
+		
+		reader.close();
+		
+	}
+	
+	private void addCrossRef(String name, String reference)
+	{
+		ImageIcon icon = iconMap.get(reference);
+		
+		if(icon == null)
+		{
+			log.error("Cross Ref for " + name + " is not valid");
+		}
+		else
+		{
+			log.debug("Icon Cross Ref " + name + " " + reference);
+			
+			iconMap.put(name, icon);
 		}
 	}
 	
+	private void addIcon(String name, String iconPath)
+	{
+		// File will replace invalid file separators chars (for current system e.g. / to \ ) with a valid system separator
+		String filePath = new File(iconPath).toString();
+		
+		log.debug(name + " " + filePath);
+		
+		iconMap.put(name, new ImageIcon(filePath, name));
+	}
 }
