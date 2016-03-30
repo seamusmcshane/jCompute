7b5871f079e889014eec332f997a8cb4586612e4
Seamus McShane <seamus.mcshane@hotmail.com>
2016-03-30 20:26:56 +0100

    Add helper class for logging unit test progress and messages.

---
+++ src/jCompute/util/JUnitTestLogger.java
@@ -0,0 +1,46 @@
+package jCompute.util;
+
+import org.junit.rules.TestWatcher;
+import org.junit.runner.Description;
+
+public class JUnitTestLogger extends TestWatcher
+{
+	@Override
+	protected void starting(Description description)
+	{
+		if(description.getMethodName() == null)
+		{
+			// Suit Top Level
+			System.out.println("Starting " + description.getDisplayName() + " - Tests : " + description.testCount());
+		}
+		else
+		{
+			System.out.println("Running Test " + description.getMethodName() + " ");
+		}
+	}
+	
+	@Override
+	protected void succeeded(Description description)
+	{
+		if(description.getMethodName() == null)
+		{
+			System.out.println("Passed " + description.getDisplayName() + " - Tests : " + description.testCount());
+		}
+		else
+		{
+			System.out.println(" Passed " + description.getMethodName() + " ");
+		}
+	}
+	
+	@Override
+	protected void failed(Throwable e, Description description)
+	{
+		System.out.print(" Failed " + e.getMessage());
+	}
+	
+	@Override
+	protected void finished(Description description)
+	{
+		System.out.println();
+	}
+}
