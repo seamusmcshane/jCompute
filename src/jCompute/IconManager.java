a1c8246be6bf5bbf89119fe31123b029390915bf
Seamus McShane <seamus.mcshane@hotmail.com>
2016-04-01 02:11:20 +0100

    Rework icon manager to avoid needing to evaluate the name of every icon before use.
    Add IconIndex enum for enabling direct lookup of icons.
    Use Pearson hash to pre-hash icon name strings, creating a perfect minimal hash table for fast integer based look.
    Allow enums to be found by the hash mapping via keeping hashes sorted by IconIndex value.
    
    Validate the icon mapping is complete. (contains icons for all IconIndex names)

--- src/jCompute/IconManager.java
+++ src/jCompute/IconManager.java
@@ -6,7 +6,6 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.net.URL;
-import java.util.HashMap;
 import java.util.LinkedList;
 
 import javax.swing.ImageIcon;
@@ -14,277 +13,360 @@ import javax.swing.ImageIcon;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import jCompute.util.PearsonHash;
+
 public final class IconManager
 {
 	// Log4j2 Logger
 	private static Logger log = LogManager.getLogger(IconManager.class);
-
-	private static HashMap<String, ImageIcon> iconMap;
-
+	
+	private static ImageIcon[] icons;
+	
 	@SuppressWarnings("unused")
 	private static IconManager iconManager;
-
+	
+	public static void initialiseWithTheme(String themeName)
+	{
+		iconManager = new IconManager(themeName);
+	}
+	
 	private IconManager(String themeName)
 	{
 		if(themeName.equals("none"))
 		{
 			return;
 		}
-
+		
 		try
 		{
-			iconMap = new HashMap<String, ImageIcon>();
-
+			// Size the array to total IconIndex enum length
+			icons = new ImageIcon[IconIndex.values().length];
+			
 			log.info("Loading icon theme " + themeName);
-
+			
 			String themeURI = "/icons/" + themeName + "/";
-
+			
 			readIconsViaMapping(themeURI);
+			
+			// Check the the icons are all loaded.
+			for(int i = 0; i < icons.length; i++)
+			{
+				if(icons[i] == null)
+				{
+					log.warn("Theme " + themeName + " has no Icon Mapping for " + IconIndex.fromValue(i).getName());
+				}
+			}
 		}
 		catch(IOException | URISyntaxException e)
 		{
 			log.warn("Cannot read mapping file for " + themeName);
 		}
 	}
-
-	public static void init(String themeName)
+	
+	/**
+	 * Retrieves the an icon from the IconManager.
+	 *
+	 * @param index
+	 * @return
+	 * null if IconManager is not initialised or there is no theme loaded.
+	 */
+	public static ImageIcon retrieveIcon(IconIndex index)
 	{
-		iconManager = new IconManager(themeName);
-	}
-
-	public static ImageIcon retrieveIcon(String iconName)
-	{
-		if(iconMap == null)
+		if(icons == null)
 		{
 			return null;
 		}
-
-		ImageIcon icon = iconMap.get(iconName);
-
-		if(icon == null)
-		{
-			log.warn("No matching icon found for " + iconName);
-
-			Thread.dumpStack();
-		}
-
-		return icon;
+		
+		return icons[index.value];
 	}
-
+	
 	/**
 	 * Mapping file format
 	 * Purpose to map original icon names to jCompute internal names and avoid needing to rename icons.
 	 * Lines starting with # are comments and ignored.
+	 * # Comment
 	 * Lines that start with a space will trigger a log warning.
 	 * Mappings declarations are comma separated name,relative path pairs
+	 * iconIndexName,relativePath
 	 * Mappings that have a relative path starting with a ! are cross reference mappings.
+	 * iconIndexName,!otherIconIndex
 	 * Cross reference mappings declarations - point to an existing mapping in the format name,!existingMappingName.
-	 * Cross reference mappings declarations can be in any location in the mapping file, including before the exiting mapping declaration.
-	 * Mapping List
-	 * ##########################################
-	 * # Interactive GUI
-	 * ##########################################
-	 * #
-	 * # Main Tabs
-	 * ##########################################
-	 * addSimTab32,
-	 * simListTab32,
-	 * #
-	 * # Simulation Tab Status
-	 * ##########################################
-	 * simTabStatusNew32,!stop32
-	 * simTabStatusRunning32,!start32
-	 * simTabStatusPaused32,!pause32
-	 * simTabStatusFinished32,
-	 * #
-	 * # Scenario Editor Buttons
-	 * ##########################################
-	 * openScenario32,
-	 * saveScenario32,
-	 * #
-	 * # Sim Control Buttons
-	 * ##########################################
-	 * generateSim16,
-	 * startSim16,!start16
-	 * pauseSim16,!paused16
-	 * resumeSim16,!resume16
-	 * #
-	 * # Simulation Tab - Tabs
-	 * ##########################################
-	 * scenarioEditor16,
-	 * chartTab16,
-	 * charts16,
-	 * #
-	 * #
-	 * ##########################################
-	 * # Batch GUI
-	 * ##########################################
-	 * #
-	 * # Main Tabs
-	 * ##########################################
-	 * batchTab32,
-	 * clusterTab32,
-	 * #
-	 * # Dynamic Tabs
-	 * ##########################################
-	 * loggingTab32,
-	 * benchmarkTab32,
-	 * #
-	 * # Batch Tab
-	 * ##########################################
-	 * addBatch32,
-	 * removeBatch32,
-	 * moveForward32,
-	 * moveBackward32,
-	 * moveToFront32,
-	 * moveToBack32,
-	 * start32,
-	 * stop32,
-	 * pause32,
-	 * #
-	 * # Batch Priority (Fifo/Fair)
-	 * ##########################################
-	 * std32,
-	 * high32,
-	 * #
-	 * # Simulation/Batch Tables Status
-	 * ##########################################
-	 * start16,
-	 * pause16,
-	 * stop16,
-	 * paused16,
-	 * resume16,
-	 * #
-	 * # Cluster Info Tabs
-	 * ##########################################
-	 * simListTab16,
-	 * loggingTab16,
-	 * nodesTab16,
+	 * Cross reference mappings declarations can be in any location in the mapping file, including before the target mapping declaration.
 	 *
 	 * @param themeURI
 	 * @throws IOException
 	 * @throws URISyntaxException
 	 */
-
 	private void readIconsViaMapping(String themeURI) throws IOException, URISyntaxException
 	{
 		LinkedList<String[]> crossRefs = new LinkedList<String[]>();
-
+		
 		// Locate the theme icons path. (Note URL)
 		URL baseURL = IconManager.class.getResource(themeURI);
-
+		
 		// Find the full path to the mapping file (URLtoPath via URI)
 		String basePath = new File(baseURL.toURI()).getAbsolutePath();
 		String mappingFilePath = basePath + File.separator + "icon.mapping";
-
-		log.info(mappingFilePath);
-
+		
+		log.info("Parsing " + mappingFilePath);
+		
+		final String lineEmptyMessage = "Empty line in mapping file on line ";
+		final String lineParseErrorMessage = "Error parsing mapping on line ";
+		final String lineIndexErrorMessage = "Did not find a matching index for mapping on line ";
+		
+		final String crossRefMappingErrorMessage = "A cross reference was not created due to the mapping not matching an index ";
+		final String crossRefReferenceErrorMessage = "A cross reference was not created due to the reference not matching an index ";
+		
+		// The last error message
+		String errorMessage = null;
+		
 		BufferedReader reader = new BufferedReader(new FileReader(new File(mappingFilePath)));
-
+		
 		String line = reader.readLine();
-
+		
 		int lineNo = 0;
-
+		
 		while(line != null)
 		{
 			boolean skipLine = false;
-
+			
+			// An invalid empty line
 			if(line.length() == 0)
 			{
-				log.warn("Got empty line in mapping file at " + lineNo);
-
+				log.warn(lineEmptyMessage + lineNo);
+				
 				skipLine = true;
 			}
-
+			
+			// A comment line
+			if((line.charAt(0) == '#'))
+			{
+				skipLine = true;
+			}
+			
 			if(!skipLine)
 			{
-				if(!(line.charAt(0) == '#'))
+				boolean error = false;
+				try
 				{
-					boolean error = false;
-					try
+					// Read the line
+					String jcName = line.substring(0, line.indexOf(','));
+					String path = line.substring(line.indexOf(',') + 1, line.length());
+					
+					if((jcName == null) || (path == null))
 					{
-						
-						// Read the line
-						String jcName = line.substring(0, line.indexOf(','));
-						String path = line.substring(line.indexOf(',') + 1, line.length());
-
-						if((jcName == null) || (path == null))
+						errorMessage = lineParseErrorMessage;
+						error = true;
+					}
+					else
+					{
+						if(path.charAt(0) == '!')
 						{
-							error = true;
+							String ref = path.substring(1, path.length());
+							
+							// Path in this case is a crossref
+							crossRefs.add(new String[]
+							{
+								jcName, ref
+							});
 						}
 						else
 						{
-							if(path.charAt(0) == '!')
+							IconIndex index = lookupMappingByName(jcName);
+							
+							if(index == null)
 							{
-								String ref = path.substring(1, path.length());
-
-								// Path in this case is a crossref
-								crossRefs.add(new String[]
-								{
-									jcName, ref
-								});
+								errorMessage = lineIndexErrorMessage;
+								
+								error = true;
 							}
 							else
 							{
-								addIcon(jcName, basePath + File.separator + path);
+								// Add the mapping
+								addIcon(index, basePath + File.separator + path);
 							}
 						}
 					}
-					catch(StringIndexOutOfBoundsException e)
-					{
-						error = true;
-					}
-
-					if(error)
-					{
-						log.error("Problem reading mapping in file at Line " + lineNo);
-					}
 				}
-				else
+				catch(StringIndexOutOfBoundsException e)
 				{
-					// a line Comment
-					// # .....
+					errorMessage = lineParseErrorMessage;
+					error = true;
 				}
-
+				
+				if(error)
+				{
+					log.error(errorMessage + lineNo);
+				}
 			}
-
+			
 			lineNo++;
 			line = reader.readLine();
 		}
-
+		
 		// Map the cross refs.
 		for(String[] crossRef : crossRefs)
 		{
-			addCrossRef(crossRef[0], crossRef[1]);
+			switch(addCrossRef(crossRef[0], crossRef[1]))
+			{
+				
+				case -10:
+				{
+					// Mapping index bad
+					log.error(crossRefMappingErrorMessage + " mapping " + crossRef[0] + " reference " + crossRef[1]);
+				}
+				break;
+				case -20:
+				{
+					// reference index bad
+					log.error(crossRefReferenceErrorMessage + " mapping " + crossRef[0] + " reference " + crossRef[1]);
+				}
+				break;
+				case 0:
+				{
+					log.debug("Created crossRef mapping " + crossRef[0] + " reference " + crossRef[1]);
+					// Created
+				}
+				break;
+			}
 		}
-
+		
+		// Mapping file parsed.
 		reader.close();
-
+		
+		log.info("Parsing Completed");
 	}
-
-	private void addCrossRef(String name, String reference)
-	{
-		ImageIcon icon = iconMap.get(reference);
-
-		if(icon == null)
-		{
-			log.error("Cross Ref for " + name + " is not valid");
-		}
-		else
-		{
-			log.debug("Icon Cross Ref " + name + " " + reference);
-
-			iconMap.put(name, icon);
-		}
-	}
-
-	private void addIcon(String name, String iconPath)
+	
+	private void addIcon(IconIndex index, String iconPath)
 	{
 		// File will replace invalid file separators chars (for current system e.g. / to \ ) with a valid system separator
 		String filePath = new File(iconPath).toString();
-
-		log.debug(name + " " + filePath);
-
-		iconMap.put(name, new ImageIcon(filePath, name));
+		
+		log.debug(index.name + " " + filePath);
+		
+		// Assign the icon
+		icons[index.value] = new ImageIcon(filePath, index.name);
+	}
+	
+	private int addCrossRef(String name, String reference)
+	{
+		IconIndex mappingIndex = lookupMappingByName(name);
+		IconIndex referenceIndex = lookupMappingByName(reference);
+		
+		if(mappingIndex == null)
+		{
+			// Mapping index bad
+			return -10;
+		}
+		
+		if(referenceIndex == null)
+		{
+			// reference index bad
+			return -20;
+		}
+		
+		// Point to the original reference - it could still be null if the reference is was not created, checked else where
+		icons[mappingIndex.value] = icons[referenceIndex.value];
+		
+		// Cross reference was created successfully
+		return 0;
+	}
+	
+	/**
+	 * Looks up the current mappings for a target string
+	 *
+	 * @param name
+	 * @return
+	 * null if the name does match an IconIndex name, or the IconIndex.
+	 */
+	private IconIndex lookupMappingByName(String name)
+	{
+		return IconIndex.fromName(name);
+	}
+	
+	/**
+	 * Allows looking up the icons by array index at runtime vs the former sting/hashmap approach
+	 *
+	 * @author Seamus McShane
+	 */
+	public enum IconIndex
+	{
+		// Enum Types
+		addSimTab32(0, "addSimTab32"), simListTab32(1, "simListTab32"), simTabStatusNew32(2, "simTabStatusNew32"),
+		simTabStatusRunning32(3, "simTabStatusRunning32"), simTabStatusPaused32(4, "simTabStatusPaused32"), simTabStatusFinished32(5, "simTabStatusFinished32"),
+		openScenario32(6, "openScenario32"), saveScenario32(7, "saveScenario32"), generateSim16(8, "generateSim16"), startSim16(9, "startSim16"),
+		pauseSim16(10, "pauseSim16"), resumeSim16(11, "resumeSim16"), scenarioEditor16(12, "scenarioEditor16"), chartTab16(13, "chartTab16"),
+		charts16(14, "charts16"), batchTab32(15, "batchTab32"), clusterTab32(16, "clusterTab32"), loggingTab32(17, "loggingTab32"),
+		benchmarkTab32(18, "benchmarkTab32"), addBatch32(19, "addBatch32"), removeBatch32(20, "removeBatch32"), moveForward32(21, "moveForward32"),
+		moveBackward32(22, "moveBackward32"), moveToFront32(23, "moveToFront32"), moveToBack32(24, "moveToBack32"), start32(25, "start32"),
+		stop32(26, "stop32"), pause32(27, "pause32"), std32(28, "std32"), high32(29, "high32"), start16(30, "start16"), pause16(31, "pause16"),
+		stop16(32, "stop16"), paused16(33, "paused16"), resume16(34, "resume16"), simListTab16(35, "simListTab16"), loggingTab16(36, "loggingTab16"),
+		nodesTab16(37, "nodesTab16");
+		
+		// IconIndex value to IconIndex lookup table
+		private static final IconIndex[] indexes = new IconIndex[IconIndex.values().length];
+		private static final PearsonHash pearsonHash;
+		
+		static
+		{
+			// Index the enums by there internal value - TODO sort by internal value before generating pearson hash.
+			for(IconIndex index : IconIndex.values())
+			{
+				// Note using the IconIndex value field not enum ordinal.
+				indexes[index.value] = index;
+			}
+			
+			String[] textList = new String[IconIndex.values().length];
+			
+			for(IconIndex index : IconIndex.values())
+			{
+				textList[index.getValue()] = index.getName();
+			}
+			
+			// Value may need adjusting when indexes increase.
+			pearsonHash = new PearsonHash(textList, 10);
+		}
+		
+		private final int value;
+		private final String name;
+		
+		private IconIndex(int value, String name)
+		{
+			this.value = value;
+			this.name = name;
+		}
+		
+		public String getName()
+		{
+			return name;
+		}
+		
+		public int getValue()
+		{
+			return value;
+		}
+		
+		// Avoidance of enum iteration at runtime at the cost of a lookup table.
+		public static IconIndex fromValue(int value)
+		{
+			return ((value >= 0) && (value < indexes.length)) ? indexes[value] : null;
+		}
+		
+		public static IconIndex fromName(String name)
+		{
+			int hash = pearsonHash.getHash(name.getBytes());
+			
+			int[] minimalKeyList = pearsonHash.getHashKeys();
+			
+			for(int i = 0; i < minimalKeyList.length; i++)
+			{
+				if(hash == minimalKeyList[i])
+				{
+					return IconIndex.fromValue(i);
+				}
+			}
+			
+			return null;
+		}
 	}
 }
