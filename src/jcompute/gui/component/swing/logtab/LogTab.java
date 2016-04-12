62fdb70566c5f5d03b5e04d32553b50bcbc9778a
Seamus McShane <seamus.mcshane@hotmail.com>
2016-04-12 17:09:42 +0100

    Auto size LogTab thread field based on look and feel.

--- src/jcompute/gui/component/swing/logtab/LogTab.java
+++ src/jcompute/gui/component/swing/logtab/LogTab.java
@@ -2,12 +2,16 @@ package jcompute.gui.component.swing.logtab;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
 
 import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.SwingConstants;
 import javax.swing.SwingUtilities;
 
 import org.apache.commons.io.input.Tailer;
@@ -16,70 +20,69 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import jcompute.gui.cluster.tablerowitems.StandardLogRowItem;
+import jcompute.gui.component.swing.SwingHelper;
 import jcompute.gui.component.swing.jpanel.SimpleTabPanel;
 import jcompute.gui.component.swing.jpanel.TablePanel;
 import jcompute.gui.component.tablecell.TextHighLighterRenderer;
 import jcompute.logging.Logging;
-
-import java.awt.GridLayout;
-import javax.swing.JLabel;
-import javax.swing.SwingConstants;
-import javax.swing.JComboBox;
+import jcompute.util.JCText;
 
 public class LogTab extends JPanel implements TailerListener
 {
 	// Log4j2 Logger
 	private static Logger log = LogManager.getLogger(LogTab.class);
-	
+
 	private static final long serialVersionUID = 758063954767436104L;
 	private Tailer tailer;
 	private Thread tailerThread;
 	private TablePanel<Integer, StandardLogRowItem> standardLog;
-	
+
 	private final int MAX_ITEMS = 5000;
 	private int linesAdded;
 	private JLabel lblLinesVar;
-	
+
 	private LogTab self = this;
-	
+
 	public LogTab(SimpleTabPanel tabpanel)
 	{
 		setLayout(new BorderLayout());
 		createTailer(self);
-		
+
 		String[] levelNames = new String[]
 		{
 			"ALL", "INFO", "WARN", "ERROR", "DEBUG"
 		};
-		
+
 		LogLevelSelect levelSector = new LogLevelSelect(levelNames);
-		
+
 		standardLog = new TablePanel<Integer, StandardLogRowItem>(StandardLogRowItem.class, "Standard Log", true, false, false, levelSector);
 		standardLog.setColumWidth(0, 80);
 		standardLog.setColumWidth(1, 80);
 		standardLog.setColumWidth(2, 80);
-		standardLog.setColumWidth(3, 180);
-		
+
+		// Size to to n chars what ever font size the LooknFeel is using .
+		standardLog.setColumWidth(3, SwingHelper.getInstance().getFontStringWidth(standardLog.getFont(), JCText.CharRepeatBounded('W', 25)));
+
 		JComboBox<String> comboBoxLevelSelect = levelSector.getJComboBox(standardLog);
-		
+
 		Color[] levelColors = new Color[]
 		{
 			Color.BLUE, Color.ORANGE, Color.RED, Color.GREEN.darker()
 		};
-		
+
 		levelNames = new String[]
 		{
 			"INFO", "WARN", "ERROR", "DEBUG"
 		};
-		
+
 		standardLog.addColumRenderer(new TextHighLighterRenderer(levelColors, levelNames), 2);
-		
+
 		this.add(standardLog, BorderLayout.CENTER);
-		
+
 		JPanel panel = new JPanel();
 		add(panel, BorderLayout.SOUTH);
 		panel.setLayout(new GridLayout(1, 6, 0, 0));
-		
+
 		JButton btnClose = new JButton("Close Tab");
 		btnClose.addActionListener(new ActionListener()
 		{
@@ -89,16 +92,16 @@ public class LogTab extends JPanel implements TailerListener
 				synchronized(self)
 				{
 					tailer.stop();
-					
+
 					tabpanel.removeTab(self);
 					standardLog.clearTable();
-					
+
 					standardLog = null;
 					tailerThread = null;
 				}
 			}
 		});
-		
+
 		JButton btnClear = new JButton("Clear View");
 		btnClear.addActionListener(new ActionListener()
 		{
@@ -113,23 +116,23 @@ public class LogTab extends JPanel implements TailerListener
 				}
 			}
 		});
-		
+
 		JPanel panel_LogLines = new JPanel();
 		panel.add(panel_LogLines);
 		panel_LogLines.setLayout(new GridLayout(0, 2, 0, 0));
-		
+
 		JLabel lblLines = new JLabel("Lines");
 		lblLines.setHorizontalAlignment(SwingConstants.CENTER);
 		panel_LogLines.add(lblLines);
-		
+
 		lblLinesVar = new JLabel("");
 		lblLinesVar.setHorizontalAlignment(SwingConstants.LEFT);
 		panel_LogLines.add(lblLinesVar);
-		
+
 		// Add JComboBox
 		panel.add(comboBoxLevelSelect);
 		panel.add(btnClear);
-		
+
 		JButton btnReload = new JButton("Reload Log");
 		btnReload.addActionListener(new ActionListener()
 		{
@@ -139,7 +142,7 @@ public class LogTab extends JPanel implements TailerListener
 				synchronized(self)
 				{
 					createTailer(self);
-					
+
 					tailerThread.start();
 				}
 			}
@@ -147,25 +150,25 @@ public class LogTab extends JPanel implements TailerListener
 		panel.add(btnReload);
 		panel.add(btnClose);
 	}
-	
+
 	private void createTailer(LogTab self)
 	{
 		if(tailer != null)
 		{
 			tailer.stop();
-			
+
 			standardLog.clearTable();
 		}
-		
+
 		linesAdded = 0;
 		refreshLabels();
-		
+
 		tailer = new Tailer(new File(Logging.getStandardLogPath()), self, 1000);
 		tailerThread = new Thread(tailer);
 		tailerThread.setDaemon(true);
 		tailerThread.setName("Logging Tailer");
 	}
-	
+
 	private void refreshLabels()
 	{
 		SwingUtilities.invokeLater(new Runnable()
@@ -177,7 +180,7 @@ public class LogTab extends JPanel implements TailerListener
 			}
 		});
 	}
-	
+
 	public void start()
 	{
 		SwingUtilities.invokeLater(new Runnable()
@@ -189,48 +192,48 @@ public class LogTab extends JPanel implements TailerListener
 			}
 		});
 	}
-	
+
 	@Override
 	public void fileNotFound()
 	{
 		
 	}
-	
+
 	@Override
 	public void fileRotated()
 	{
 		
 	}
-	
+
 	@Override
 	public void handle(String line)
 	{
 		int start = line.indexOf('|', 0) + 2;
 		int end = line.indexOf('|', start) - 1;
 		String sindex = line.substring(start, end);
-		
+
 		int index = Integer.parseInt(sindex);
-		
+
 		start = line.indexOf('|', end) + 2;
 		end = line.indexOf('[', start) - 1;
 		String level = line.substring(start, end);
-		
+
 		start = end + 2;
 		end = line.indexOf(']', start);
 		String comp = line.substring(start, end);
-		
+
 		start = line.indexOf('-', end) + 1;
 		end = line.length();
 		String message = line.substring(start, end);
-		
+
 		synchronized(self)
 		{
 			// System.out.println("TAILER " + index + " " + level + " " + comp + " " + message + " start " + start + " " + " end " + end);
 			standardLog.addRowTailed(new StandardLogRowItem(++linesAdded, index, level, comp, message), MAX_ITEMS);
-			
+
 			refreshLabels();
 		}
-		
+
 		SwingUtilities.invokeLater(new Runnable()
 		{
 			@Override
@@ -240,13 +243,13 @@ public class LogTab extends JPanel implements TailerListener
 			}
 		});
 	}
-	
+
 	@Override
 	public void handle(Exception e)
 	{
 		
 	}
-	
+
 	@Override
 	public void init(Tailer e)
 	{
