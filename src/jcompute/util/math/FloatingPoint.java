384181c50f5b44498c964cd5e4a50eaba2bfe12f
Seamus McShane <seamus.mcshane@hotmail.com>
2016-06-30 17:39:34 +0100

    Add FloatingPoint equality helper functions.

---
+++ src/jcompute/util/math/FloatingPoint.java
@@ -0,0 +1,16 @@
+package jcompute.util.math;
+
+public class FloatingPoint
+{
+	public final static float FLOAT_EPSILON = 1.0f / 8192f;
+	
+	public static float Abs(float val)
+	{
+		return (val <= 0) ? 0 - val : val;
+	}
+	
+	public static boolean AlmostEqual(float a, float b)
+	{
+		return(Math.abs(a - b) < FLOAT_EPSILON);
+	}
+}
