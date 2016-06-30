9ee5ab274494181f3b5b1e5662bee46794801ee2
Seamus McShane <seamus.mcshane@hotmail.com>
2016-06-30 17:37:47 +0100

    Add contains x,y to A2DRectangle

--- src/jcompute/gui/view/graphics/A2DRectangle.java
+++ src/jcompute/gui/view/graphics/A2DRectangle.java
@@ -8,85 +8,113 @@ public class A2DRectangle
 	private float width;
 	private float height;
 	private A2RGBA color;
-		
+	
 	public A2DRectangle(float x, float y, float width, float height)
 	{
 		super();
-
+		
 		this.width = width;
 		this.height = height;
 		
 		setColor(new A2RGBA(Color.white));
 		
-		setLocation(x,y);
+		setLocation(x, y);
 	}
 	
-	public A2DRectangle(float x, float y, float width, float height,A2RGBA color)
+	public A2DRectangle(float x, float y, float width, float height, A2RGBA color)
 	{
 		super();
-
+		
 		this.width = width;
 		this.height = height;
 		
 		this.setColor(color);
 		
-		setLocation(x,y);
+		setLocation(x, y);
 	}
-
-	public boolean contains(float[] pos)
+	
+	public boolean contains(float x, float y)
 	{
 		// outside left wall?
-		if(pos[0]<coords[0])
+		if(x < coords[0])
 		{
 			return false;
 		}
 		
 		// outside bottom wall
-		if(pos[1]<coords[1])
+		if(y < coords[1])
 		{
 			return false;
 		}
 		
 		// outside right wall
-		if(pos[0]>coords[2])
+		if(x > coords[2])
 		{
 			return false;
 		}
 		
-		if(pos[1]>coords[3])
+		if(y > coords[3])
 		{
 			return false;
 		}
 		
-		return true;	
+		return true;
+	}
+	
+	public boolean contains(float[] pos)
+	{
+		// outside left wall?
+		if(pos[0] < coords[0])
+		{
+			return false;
+		}
+		
+		// outside bottom wall
+		if(pos[1] < coords[1])
+		{
+			return false;
+		}
+		
+		// outside right wall
+		if(pos[0] > coords[2])
+		{
+			return false;
+		}
+		
+		if(pos[1] > coords[3])
+		{
+			return false;
+		}
+		
+		return true;
 	}
 	
 	public boolean contains(double[] pos)
 	{
 		// outside left wall?
-		if(pos[0]<coords[0])
+		if(pos[0] < coords[0])
 		{
 			return false;
 		}
 		
 		// outside bottom wall
-		if(pos[1]<coords[1])
+		if(pos[1] < coords[1])
 		{
 			return false;
 		}
 		
 		// outside right wall
-		if(pos[0]>coords[2])
+		if(pos[0] > coords[2])
 		{
 			return false;
 		}
 		
-		if(pos[1]>coords[3])
+		if(pos[1] > coords[3])
 		{
 			return false;
 		}
 		
-		return true;	
+		return true;
 	}
 	
 	public float getX()
@@ -109,20 +137,20 @@ public class A2DRectangle
 		return height;
 	}
 	
-	public void setLocation(float x,float y)
+	public void setLocation(float x, float y)
 	{
 		coords = new float[4];
 		coords[0] = x;
 		coords[1] = y;
-		coords[2] = x+width;
-		coords[3] = y+height;		
+		coords[2] = x + width;
+		coords[3] = y + height;
 	}
-
+	
 	public A2RGBA getColor()
 	{
 		return color;
 	}
-
+	
 	public void setColor(A2RGBA color)
 	{
 		this.color = color;
@@ -154,51 +182,51 @@ public class A2DRectangle
 	
 	public float getXAxisMin()
 	{
-		if(coords[0]<coords[2])
+		if(coords[0] < coords[2])
 		{
 			return coords[0];
 		}
 		else
 		{
 			return coords[2];
-
-		}		
+			
+		}
 	}
 	
 	public float getYAxisMin()
 	{
-		if(coords[1]<coords[3])
+		if(coords[1] < coords[3])
 		{
 			return coords[1];
 		}
 		else
 		{
 			return coords[3];
-		}		
+		}
 	}
-
+	
 	public float getXAxisMax()
 	{
-		if(coords[0]>coords[2])
+		if(coords[0] > coords[2])
 		{
 			return coords[0];
 		}
 		else
 		{
 			return coords[2];
-
-		}		
+			
+		}
 	}
 	
 	public float getYAxisMax()
 	{
-		if(coords[1]>coords[3])
+		if(coords[1] > coords[3])
 		{
 			return coords[1];
 		}
 		else
 		{
 			return coords[3];
-		}		
+		}
 	}
 }
