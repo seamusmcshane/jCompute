b29ba3f7ceec84b2abe3993df604bc85649220ff
Seamus McShane <seamus.mcshane@hotmail.com>
2016-06-30 17:39:34 +0100

    Add lookup table based trig functions.

---
+++ src/jcompute/util/math/JCTrig.java
@@ -0,0 +1,219 @@
+package jcompute.util.math;
+
+import org.apache.commons.math3.util.FastMath;
+
+public class JCTrig
+{
+	// Must be base 2 for fast modulus
+	private static final int VALUES = (int) Math.pow(2, 10);
+	private static final int VALUESM1 = VALUES - 1;
+	private static final int HALF_VALUES = VALUES / 2;
+	
+	public static final double PI = Math.PI;
+	public static final float PI_F = (float) Math.PI;
+	
+	public static final double HALF_PI = (Math.PI / 2);
+	public static final double QUATER_PI = (Math.PI / 4);
+	public static final double PI_PLUS_HALF_PI = (3 * (Math.PI / 4));
+	public static final double TWO_PI = (2 * Math.PI);
+	
+	private static final double[] sineTable;
+	private static final double SINE_TABLE_STEP_SIZE = (TWO_PI / VALUES);
+	private static final double REV_SINE_TABLE_STEP_SIZE = (VALUES / TWO_PI);
+	
+	private static final double[] asineTable;
+	private static final double A_TABLE_STEP_SIZE = (2.0 / VALUES);
+	private static final double REV_A_TABLE_STEP_SIZE = (VALUES / 2.0);
+	
+	static
+	{
+		// These lookup tables only get generated if the class is accessed/loaded.
+		// Java.Math used for generation
+		sineTable = new double[VALUES];
+		
+		// Min
+		sineTable[0] = Math.sin(0);
+		
+		for(int s = 1; s < VALUESM1; s++)
+		{
+			sineTable[s] = (Math.sin(SINE_TABLE_STEP_SIZE * s));
+		}
+		
+		// Max
+		sineTable[VALUESM1] = Math.sin(TWO_PI);
+		
+		asineTable = new double[VALUES];
+		
+		// Min
+		asineTable[0] = Math.asin(-1);
+		
+		for(int s = 1; s < VALUESM1; s++)
+		{
+			asineTable[s] = Math.asin((A_TABLE_STEP_SIZE * s) - 1.0);
+		}
+		
+		// Max
+		asineTable[VALUESM1] = Math.asin(1);
+		
+		System.out.println("JCTrig Lookup Table Size + " + VALUES);
+	}
+	
+	/**
+	 * Returns an approximate sin value using a look up table.
+	 * From the direct index look up and the next index linear interpolation is performed.
+	 * 
+	 * @param x
+	 * @return
+	 */
+	public static double sinLutInt(double x)
+	{
+		double offset = x * REV_SINE_TABLE_STEP_SIZE;
+		int intOffset = (int) Math.floor(offset);
+		offset -= intOffset;
+		
+		int index1 = intOffset & VALUESM1;
+		int index2 = (intOffset + 1) & VALUESM1;
+		
+		double sin1 = sineTable[index1];
+		double sin2 = sineTable[index2];
+		
+		return(((1.0 - offset) * sin1) + (sin2 * offset));
+	}
+	
+	/**
+	 * Returns an approximate cos value using a look up table.
+	 * From the direct index look up and the next index linear interpolation is performed.
+	 * 
+	 * @param x
+	 * @return
+	 */
+	public static double cosLutInt(double x)
+	{
+		return sinLutInt(x + HALF_PI);
+	}
+	
+	/**
+	 * Returns an approximate tan value using a look up table.
+	 * From the direct index look up and the next index linear interpolation is performed.
+	 * 
+	 * @param x
+	 * @return
+	 */
+	public static double tanLutInt(double x)
+	{
+		double cos = (sinLutInt(x + HALF_PI));
+		
+		// sin/cos
+		return (cos == 0) ? cos : sinLutInt(x) / cos;
+	}
+	
+	/**
+	 * Returns an approximate asin value using a look up table.
+	 * From the direct index look up and the next index linear interpolation is performed.
+	 * 
+	 * @param x
+	 * @return
+	 */
+	public static double asinLutInt(double x)
+	{
+		// ASIN does not wrap so we must check the bounds
+		// This should return nans for impossible values
+		
+		// Invalid values given - this also prevents our array index under/overflowing.
+		if(x < -1 || x > 1)
+		{
+			return Double.NaN;
+		}
+		
+		// Use absolute output values for absolute input values
+		if(x == 1)
+		{
+			return HALF_PI;
+		}
+		
+		if(x == -1)
+		{
+			return -HALF_PI;
+		}
+		
+		// Signed zero is preserved per IEEE754
+		if(x == 0)
+		{
+			return x;
+		}
+		
+		double offset = (x * REV_A_TABLE_STEP_SIZE) + HALF_VALUES;
+		int intOffset = (int) Math.floor(offset);
+		offset -= intOffset;
+		
+		int index1 = intOffset;
+		int index2 = (intOffset + 1);
+		
+		// Cannot interpolate +1 - index1 is at the array length
+		if(index1 == VALUESM1)
+		{
+			return asineTable[index1];
+		}
+		
+		double asin1 = asineTable[index1];
+		double asin2 = asineTable[index2];
+		
+		// System.out.println("index 1 + " + index1 + " asin1 " + asin1 + " index2 " + index2 + " asin2 " + asin2);
+		
+		return(((1.0 - offset) * asin1) + (asin2 * offset));
+	}
+	
+	/**
+	 * Returns an approximate acos value using a look up table.
+	 * From the direct index look up and the next index linear interpolation is performed.
+	 * 
+	 * @param x
+	 * @return
+	 */
+	public static double acosLutInt(double x)
+	{
+		return HALF_PI - asinLutInt(x);
+	}
+	
+	/**
+	 * Delegates to Apache FastMath.atan2
+	 * 
+	 * @param y
+	 * @param x
+	 * @return
+	 */
+	public static float atan2Float(float y, float x)
+	{
+		return (float) FastMath.atan2(y, x);
+	}
+	
+	/**
+	 * Delegates to Apache FastMath.atan
+	 * 
+	 * @param y
+	 * @param x
+	 * @return
+	 */
+	public static float atanFloat(float x)
+	{
+		return (float) FastMath.atan(x);
+	}
+	
+	/**
+	 * @param angleDegree
+	 * @return
+	 */
+	public static float toRadiansFloat(float angleDegree)
+	{
+		return angleDegree / 180.0f * PI_F;
+	}
+	
+	/**
+	 * @param angleRadians
+	 * @return
+	 */
+	public static float toDegreesFloat(float angleRadians)
+	{
+		return angleRadians * 180.0f / PI_F;
+	}
+}
---
+++ src/jcompute/util/math/TrigTest.java
@@ -0,0 +1,294 @@
+package jcompute.util.math;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+import jcompute.timing.TimerObj;
+import jcompute.util.JCText;
+
+public class TrigTest
+{
+	public static void main(String args[])
+	{
+		int vals = 3600;
+		double stepSize = ((2 * Math.PI) / vals);
+		double invStepSize = 2.0 / vals;
+		
+		System.out.println("stepSize " + stepSize);
+		System.out.println("invStepSize " + invStepSize);
+		
+		int size = 20;
+		
+		printDouble(Math.sin(0));
+		printDouble(Math.sin(Math.PI / 2));
+		printDouble(Math.sin(3 * (Math.PI / 2)));
+		printDouble(Math.sin(4 * (Math.PI / 2)));
+		
+		printDouble(JCTrig.sinLutInt(0));
+		printDouble(JCTrig.sinLutInt((Math.PI / 2)));
+		printDouble(JCTrig.sinLutInt((3 * (Math.PI / 2))));
+		printDouble(JCTrig.sinLutInt((4 * (Math.PI / 2))));
+		
+		double warmup = 0;
+		for(int i = 0; i < 200000 + 1; i++)
+		{
+			warmup += Math.sin(i);
+			warmup += JCTrig.sinLutInt(i);
+			warmup += Math.cos(i);
+			warmup += JCTrig.cosLutInt(i);
+			warmup += Math.tan(i);
+			warmup += JCTrig.tanLutInt(i);
+		}
+		
+		System.out.println("Warmup " + warmup);
+		//
+		// System.out.println("Sin x " + JCText.SpacePaddedString(0, size) + " : " + JCText.SpacePaddedString("Math.sin(x)", size) + " " + JCText
+		// .SpacePaddedString("JCTrig.sinLut(x)", size) + " " + JCText.SpacePaddedString("JCTrig.sinLutInt(x)", size));
+		//
+		// for(int i = 0; i <= vals; i++)
+		// {
+		// double x = (stepSize * i);
+		//
+		// System.out.println("Sin x " + JCText.SpacePaddedString(x, size) + " : " + JCText.SpacePaddedString(Math.sin(x), size) + " " + JCText
+		// .SpacePaddedString(JCTrig.sinLutInt(x), size) + " DIFF " + (Math.sin(x) - JCTrig.sinLutInt(x)));
+		// }
+		//
+		// for(int i = 0; i <= vals; i++)
+		// {
+		// double x = (stepSize * i);
+		//
+		// System.out.println("Cos x " + JCText.SpacePaddedString(x, size) + " : " + JCText.SpacePaddedString(Math.cos(x), size) + " " + JCText
+		// .SpacePaddedString(JCTrig.cosLutInt(x), size) + " DIFF " + (Math.cos(x) - JCTrig.cosLutInt(x)));
+		// }
+		//
+		// for(int i = 0; i <= vals; i++)
+		// {
+		// double x = (stepSize * i);
+		//
+		// System.out.println("Tan x " + JCText.SpacePaddedString(x, size) + " : " + JCText.SpacePaddedString(Math.tan(x), size) + " " + JCText
+		// .SpacePaddedString(JCTrig.tanLutInt(x), size) + " DIFF " + (Math.tan(x) - JCTrig.tanLutInt(x)));
+		// }
+		
+		for(int i = 0; i <= vals; i++)
+		{
+			double x = -1.0 + (invStepSize * i);
+			
+			System.out.println("ASin x " + JCText.SpacePaddedString(x, size) + " : " + JCText.SpacePaddedString(Math.asin(x), size) + " " + JCText
+			.SpacePaddedString(JCTrig.asinLutInt(x), size) + " DIFF " + (Math.asin(x) - JCTrig.asinLutInt(x)));
+		}
+		
+		for(int i = 0; i < vals + 1; i++)
+		{
+			double x = -1.0 + (invStepSize * i);
+			
+			System.out.println("ACos x " + JCText.SpacePaddedString(x, size) + " : " + JCText.SpacePaddedString(Math.acos(x), size) + " " + JCText
+			.SpacePaddedString(JCTrig.acosLutInt(x), size) + " DIFF " + (Math.acos(x) - JCTrig.acosLutInt(x)));
+		}
+		
+		int iter = 100000000;
+		
+		System.out.println("Iterations " + iter);
+		testSin(iter);
+		testCos(iter);
+		testTan(iter);
+		testASin(iter);
+		testACos(iter);
+	}
+	
+	public static void testSin(int iter)
+	{
+		int range = 360;
+		
+		ArrayList<Integer> orders = new ArrayList<Integer>();
+		
+		for(int i = 0; i < range; i++)
+		{
+			orders.add(i);
+		}
+		
+		Collections.shuffle(orders);
+		
+		TimerObj to = new TimerObj();
+		to.startTimer();
+		
+		double dval = 0;
+		for(int i = 0; i < iter + 1; i++)
+		{
+			dval += Math.sin(orders.get(i % range));
+		}
+		to.stopTimer();
+		
+		System.out.println("Math.sin " + to.getTimeTaken() + " " + dval);
+		
+		dval = 0;
+		
+		to.startTimer();
+		
+		for(int i = 0; i < iter + 1; i++)
+		{
+			dval += JCTrig.sinLutInt(orders.get(i % range));
+		}
+		to.stopTimer();
+		
+		System.out.println("JCTrig.sinLutInt " + to.getTimeTaken() + " " + dval);
+	}
+	
+	public static void testCos(int iter)
+	{
+		int range = 360;
+		
+		ArrayList<Integer> orders = new ArrayList<Integer>();
+		
+		for(int i = 0; i < range; i++)
+		{
+			orders.add(i);
+		}
+		
+		Collections.shuffle(orders);
+		
+		TimerObj to = new TimerObj();
+		to.startTimer();
+		
+		double dval = 0;
+		for(int i = 0; i < iter + 1; i++)
+		{
+			dval += Math.cos(orders.get(i % range));
+		}
+		to.stopTimer();
+		
+		System.out.println("Math.cos " + to.getTimeTaken() + " " + dval);
+		
+		dval = 0;
+		
+		to.startTimer();
+		
+		for(int i = 0; i < iter + 1; i++)
+		{
+			dval += JCTrig.cosLutInt(orders.get(i % range));
+		}
+		to.stopTimer();
+		
+		System.out.println("JCTrig.cosLutInt " + to.getTimeTaken() + " " + dval);
+	}
+	
+	public static void testTan(int iter)
+	{
+		int range = 360;
+		
+		ArrayList<Integer> orders = new ArrayList<Integer>();
+		
+		for(int i = 0; i < range; i++)
+		{
+			orders.add(i);
+		}
+		
+		Collections.shuffle(orders);
+		
+		TimerObj to = new TimerObj();
+		to.startTimer();
+		
+		double dval = 0;
+		for(int i = 0; i < iter + 1; i++)
+		{
+			dval += Math.tan(orders.get(i % range));
+		}
+		to.stopTimer();
+		
+		System.out.println("Math.tan " + to.getTimeTaken() + " " + dval);
+		
+		dval = 0;
+		
+		to.startTimer();
+		
+		for(int i = 0; i < iter + 1; i++)
+		{
+			dval += JCTrig.tanLutInt(orders.get(i % range));
+		}
+		to.stopTimer();
+		
+		System.out.println("JCTrig.tanLutInt " + to.getTimeTaken() + " " + dval);
+	}
+	
+	public static void testASin(int iter)
+	{
+		int range = 1000;
+		double scale = 1000000.0;
+		
+		ArrayList<Integer> orders = new ArrayList<Integer>();
+		
+		for(int i = 0; i < range; i++)
+		{
+			orders.add(i);
+		}
+		
+		Collections.shuffle(orders);
+		
+		TimerObj to = new TimerObj();
+		to.startTimer();
+		
+		double dval = 0;
+		for(int i = 0; i < iter + 1; i++)
+		{
+			dval += Math.asin(orders.get(i % range) / scale);
+		}
+		to.stopTimer();
+		
+		System.out.println("Math.asin " + to.getTimeTaken() + " " + dval);
+		
+		dval = 0;
+		
+		to.startTimer();
+		
+		for(int i = 0; i < iter + 1; i++)
+		{
+			dval += JCTrig.asinLutInt(orders.get(i % range) / scale);
+		}
+		to.stopTimer();
+		
+		System.out.println("JCTrig.asinLutInt " + to.getTimeTaken() + " " + dval);
+	}
+	
+	public static void testACos(int iter)
+	{
+		int range = 1000;
+		double scale = 1000000.0;
+		
+		ArrayList<Integer> orders = new ArrayList<Integer>();
+		
+		for(int i = 0; i < range; i++)
+		{
+			orders.add(i);
+		}
+		
+		Collections.shuffle(orders);
+		
+		TimerObj to = new TimerObj();
+		to.startTimer();
+		
+		double dval = 0;
+		for(int i = 0; i < iter + 1; i++)
+		{
+			dval += Math.acos(orders.get(i % range) / scale);
+		}
+		to.stopTimer();
+		
+		System.out.println("Math.acos " + to.getTimeTaken() + " " + dval);
+		
+		dval = 0;
+		
+		to.startTimer();
+		
+		for(int i = 0; i < iter + 1; i++)
+		{
+			dval += JCTrig.acosLutInt(orders.get(i % range) / scale);
+		}
+		to.stopTimer();
+		
+		System.out.println("JCTrig.acosLutInt " + to.getTimeTaken() + " " + dval);
+	}
+	
+	public static void printDouble(double d)
+	{
+		System.out.printf("%f\n", d);
+	}
+}
