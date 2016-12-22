66ce382e44333923e2707be65590ca2975dcb37e
Seamus McShane <seamus.mcshane@hotmail.com>
2016-12-22 21:45:38 +0000

    Fix compare lookandfeels compare when lowercase.

--- src/jcompute/util/LookAndFeel.java
+++ src/jcompute/util/LookAndFeel.java
@@ -3,19 +3,31 @@ package jcompute.util;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class LookAndFeel
 {
+	private static Logger log = LogManager.getLogger(LookAndFeel.class);
+	
 	/* If possible set the requested look and feel else use system default */
 	public static void setLookandFeel(String conflookandfeel)
 	{
-		// Default to the system provided look and feel
+		// To simplify name matching.
+		String lcaseName = conflookandfeel.toLowerCase();
+		
+		// Default to the system provided look and feel (But don't set it)
 		String lookandfeel = UIManager.getSystemLookAndFeelClassName();
-		
+
 		UIManager.LookAndFeelInfo[] lookAndFeels = UIManager.getInstalledLookAndFeels();
-		
+				
 		for(int i = 0; i < lookAndFeels.length; i++)
 		{
-			if(lookAndFeels[i].getClassName().toLowerCase().contains(conflookandfeel))
+			String name = lookAndFeels[i].getClassName().toLowerCase();
+			
+			log.info("Look : " + name);
+			
+			if(name.contains(lcaseName))
 			{
 				lookandfeel = lookAndFeels[i].getClassName();
 				break;
@@ -42,5 +54,7 @@ public class LookAndFeel
 		{
 			e1.printStackTrace();
 		}
+		
+		log.info("Selected : " + lookandfeel);
 	}
 }
