e2a7c4376720b75f506d407cc9d9f503a476b5ed
Seamus McShane <>
2019-06-18 12:35:05 +0100

    Add test launcher with bean to configure Jetty.

--- src/jcompute/webinterface/WebInterfaceLauncher.java
+++ src/jcompute/webinterface/WebInterfaceLauncher.java
@@ -1,13 +1,28 @@
 package jcompute.webinterface;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 
+@EnableAutoConfiguration
 @SpringBootApplication
 public class WebInterfaceLauncher
 {
 	public static void main(String args[])
+	{		
+		SpringApplication.run(WebInterfaceLauncher.class);
+	}
+	
+	@Bean
+	public ConfigurableServletWebServerFactory webServerFactory()
 	{
-		SpringApplication.run(WebInterface.class);
+	    JettyServletWebServerFactory factory = new JettyServletWebServerFactory();
+	    factory.setPort(8080);
+	    factory.setContextPath("");
+	    //factory.addErrorPages(new ErrorPage(HttpStatus.NOT_FOUND, "/notfound.html"));
+	    return factory;
 	}
 }
