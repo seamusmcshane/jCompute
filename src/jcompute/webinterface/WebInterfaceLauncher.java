b061a066db47768b2ca94c6a61188803ddf6a06e
Seamus McShane <>
2019-06-18 11:33:21 +0100

    Add test launcher for spring boot app.

---
+++ src/jcompute/webinterface/WebInterfaceLauncher.java
@@ -0,0 +1,13 @@
+package jcompute.webinterface;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebInterfaceLauncher
+{
+	public static void main(String args[])
+	{
+		SpringApplication.run(WebInterface.class);
+	}
+}
