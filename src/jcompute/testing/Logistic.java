8da06533c2280e42b1de565376b55214e730ad25
Seamus McShane <>
2019-09-19 12:29:57 +0100

    Remove large amount of unused imports in jcompute.testing.Logistic.

--- src/jcompute/testing/Logistic.java
+++ src/jcompute/testing/Logistic.java
@@ -3,27 +3,14 @@ package jcompute.testing;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
-import java.awt.Toolkit;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
 
-import javax.imageio.ImageIO;
-import javax.swing.JButton;
-import javax.swing.JFileChooser;
 import javax.swing.JFrame;
-import javax.swing.JOptionPane;
 import javax.swing.JPanel;
-import javax.swing.JToolBar;
 
-import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.Font;
 
 public class Logistic
 {
